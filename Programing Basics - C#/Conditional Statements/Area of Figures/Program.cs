92a386c7cdea3b996422249a5215b7b1deda0d05
TSDilov <cv.dilov@abv.bg>
2021-09-09 12:38:58 +0300

    Added PB code

---
+++ Programing Basics - C#/Conditional Statements/Area of Figures/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AreaofFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fig = Console.ReadLine();
+            double area = 0.0;
+            if (fig == "square")
+            {
+                double sideS = double.Parse(Console.ReadLine());
+                area = sideS * sideS;
+            }
+            else if (fig == "rectangle")
+            {
+                double sideR1 = double.Parse(Console.ReadLine());
+                double sideR2 = double.Parse(Console.ReadLine());
+                area = sideR1 * sideR2;
+            }
+            else if (fig == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = Math.PI*radius*radius;
+            }
+            else if (fig == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double Hside = double.Parse(Console.ReadLine());
+                area = (side*Hside)/2;
+            }
+            Console.WriteLine($"{area:F3}");
+        }
+    }
+}
---
+++ Programing Basics - C#/Conditional Statements/Toy Shop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceH = double.Parse(Console.ReadLine());
+            int puz = int.Parse(Console.ReadLine());
+            int doll = int.Parse(Console.ReadLine());
+            int bear = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+            int truck = int.Parse(Console.ReadLine());
+            int num = puz + doll + bear + min + truck;
+            double sum = (puz * 2.60) + (doll * 3) + (bear * 4.10) + (min * 8.20) + (truck * 2);
+            if (num >= 50)
+            {
+                double withdiscount = sum - (sum * 1 / 4);
+                double sumForRent = withdiscount * 1 /10;
+                double profit = withdiscount - sumForRent;
+                if (profit >= priceH)
+                {
+                    double high = profit - priceH;
+                    Console.WriteLine($"Yes! {high:F2} lv left.");
+                }
+                else
+                {
+                    double low = priceH - profit;
+                    Console.WriteLine($"Not enough money! {low:F2} lv needed.");
+                }
+            }
+            else
+            {
+                double sumForRent2 = sum * 1 / 10;
+                double profit2 = sum - sumForRent2;
+                if (profit2 > priceH)
+                {
+                    double high2 = profit2 - priceH;
+                    Console.WriteLine($"Yes! {high2:F2} lv left.");
+                }
+                else
+                {
+                    double low2 = priceH - profit2;
+                    Console.WriteLine($"Not enough money! {low2:F2} lv needed.");
+                }
+            }        
+        }
+    }
+}
