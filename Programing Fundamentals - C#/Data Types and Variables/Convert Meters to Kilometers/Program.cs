642c5e287afc99c51a02d8d4b27ffff3a8ed8bbf
TSDilov <cv.dilov@abv.bg>
2021-09-10 11:02:45 +0300

    Added Fundamentals Code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConvertMeterstoKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal meters = decimal.Parse(Console.ReadLine());
+            decimal km = meters / 1000;
+            Console.WriteLine($"{km:F2}");
+        }
+    }
+}
---
+++ Programing Fundamentals - C#/Data Types and Variables/Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PoundstoDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pound = decimal.Parse(Console.ReadLine());
+            decimal dollar = pound * 1.31m;
+            Console.WriteLine($"{dollar:F3}");
+        }
+    }
+}
