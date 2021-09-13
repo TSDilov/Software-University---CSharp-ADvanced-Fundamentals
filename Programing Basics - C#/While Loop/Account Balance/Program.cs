38da10c429f59e1383ef6b40b1877a382e0463e6
TSDilov <cv.dilov@abv.bg>
2021-09-13 12:06:00 +0300

    Added PB While Loop code

---
+++ Programing Basics - C#/While Loop/Account Balance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string noMore = Console.ReadLine();
+            double total = 0;
+            while (noMore != "NoMoreMoney")
+            {
+                double sum = double.Parse(noMore);
+                if (sum < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                total += sum;
+                Console.WriteLine($"Increase: {sum:F2}");
+                noMore = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {total:F2}");
+        }
+    }
+}
