d05eeeae46ae01a54656b8ace4e20cdf1a58fb65
TSDilov <cv.dilov@abv.bg>
2021-09-14 21:31:45 +0300

    Added PB While Loop Code

---
+++ Programing Basics - C#/While Loop/Old Books/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int count = 0;
+            string nextBook = Console.ReadLine();
+            while (nextBook != "No More Books")
+            {
+                if (nextBook == book)
+                {
+                    Console.WriteLine($"You checked {count} books and found it.");
+                    return;
+                }
+                count++;
+                nextBook = Console.ReadLine();
+            }
+           
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {count} books.");          
+        }
+    }
+}
