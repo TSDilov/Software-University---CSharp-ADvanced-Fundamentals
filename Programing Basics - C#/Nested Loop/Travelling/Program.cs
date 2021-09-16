f69fd24aa5280310cc5592cc43954b4e8ed8c126
TSDilov <cv.dilov@abv.bg>
2021-09-16 15:34:24 +0300

    Added PB Nested Loop code

---
+++ Programing Basics - C#/Nested Loop/Travelling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            double budget = 0.0;
+            double save = 0.0;
+            while (destination != "End")
+            {
+                budget = double.Parse(Console.ReadLine());
+                while (save< budget)
+                {
+                    save += double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"Going to {destination}!");
+                save = 0.0;
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
