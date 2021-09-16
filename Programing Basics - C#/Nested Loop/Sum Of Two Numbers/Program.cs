daa4577b9d638e5347dd3bb89a0c78c238a11714
TSDilov <cv.dilov@abv.bg>
2021-09-16 15:32:24 +0300

    Added PB Nested loop code

---
+++ Programing Basics - C#/Nested Loop/Sum Of Two Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int magic = int.Parse(Console.ReadLine());
+            int count = 0;
+            bool flag = false;
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = a; j <= b; j++)
+                {
+                    count++;
+                    if (i + j == magic)
+                    {
+                        Console.WriteLine($"Combination N:{count} ({i} + {j} = {magic})");
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine($"{count} combinations - neither equals {magic}");
+            }
+        }
+    }
+}
