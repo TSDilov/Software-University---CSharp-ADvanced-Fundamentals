b4bd3465ad939b638ea3ab1c043286e3ccfde14a
TSDilov <cv.dilov@abv.bg>
2021-09-17 09:43:28 +0300

    Added PB Nasted Loop code

---
+++ Programing Basics - C#/Nested Loop/Sum Prime Non Prime/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SumPrimeNonPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stop = Console.ReadLine();
+            int num;
+            int primeSum = 0;
+            int nonPrimeSum = 0;
+
+            while (stop != "stop")
+            {
+                num = int.Parse(stop);
+                bool nonPrime = false;
+                if (num < 0)
+                {
+                    Console.WriteLine("Number is negative.");                  
+                }
+                else
+                {
+                    if (num == 0 || num == 1)
+                    {
+                        nonPrime = true;
+                        nonPrimeSum += num;
+                    }
+                    else
+                    {
+                        for (int i = 2; i < num; i++)
+                        {
+                            if (num % i == 0)
+                            {
+                                nonPrime = true;
+                                nonPrimeSum += num;
+                                break;
+                            }
+                        }
+                        if (!nonPrime)
+                        {
+                            primeSum += num;
+                        }
+                    }
+                }
+
+                stop = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+        }
+    }
+}
