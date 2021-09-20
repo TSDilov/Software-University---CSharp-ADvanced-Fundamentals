b92e7c3480b082f228b2ad04c64c3709a8446fc6
TSDilov <cv.dilov@abv.bg>
2021-09-20 19:53:05 +0300

    Added PB Nested Loop code

---
+++ Programing Basics - C#/Nested Loop/Train the Trainers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int judges = int.Parse(Console.ReadLine());
+            string finish = Console.ReadLine();
+            double allMarksForPresentation = 0;
+            double allMarks = 0;
+            int countMarks = 0;
+            while (finish != "Finish")
+            {
+                double mark;
+                for (int i = 1; i <= judges; i++)
+                {
+                    mark = double.Parse(Console.ReadLine());
+                    allMarksForPresentation +=mark;
+                    allMarks += mark;
+                    countMarks++;   
+                }
+                Console.WriteLine($"{finish} - {allMarksForPresentation / judges:F2}.");
+                finish = Console.ReadLine();
+                allMarksForPresentation = 0;
+            }
+            Console.WriteLine($"Student's final assessment is {allMarks / countMarks :F2}.");
+        }
+    }
+}
