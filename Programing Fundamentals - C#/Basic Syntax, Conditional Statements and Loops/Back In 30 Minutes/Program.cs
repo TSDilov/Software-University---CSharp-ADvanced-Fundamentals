2a8add00a0b1ca850b23a25cda2412ff397e5ce3
TSDilov <cv.dilov@abv.bg>
2021-09-20 20:41:44 +0300

    Added Fundamentals Basic Syntax code

---
+++ Programing Fundamentals - C#/Basic Syntax, Conditional Statements and Loops/Back In 30 Minutes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+            int minAll = hour * 60 + min;
+            int hourNew = (minAll + 30) / 60;
+            int minNew = (minAll + 30) % 60;
+            if (minAll >= 1410)
+            {
+                hourNew = 0;
+            }
+            Console.WriteLine($"{hourNew}:{minNew:D2}");
+        }
+    }
+}
