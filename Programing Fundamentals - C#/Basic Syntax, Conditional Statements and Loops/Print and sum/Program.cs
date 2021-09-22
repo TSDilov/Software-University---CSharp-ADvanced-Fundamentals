595f988ee8204ad06c41b6ed0be7bb7d6bec7d78
TSDilov <cv.dilov@abv.bg>
2021-09-22 10:55:38 +0300

    Added Fundamentals Basic Syntax code

---
+++ Programing Fundamentals - C#/Basic Syntax, Conditional Statements and Loops/Print and sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Printandsum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int finishNum = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = startNum; i <= finishNum; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
