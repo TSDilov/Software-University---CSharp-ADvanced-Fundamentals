cbda14e204749e78f64d395364ef11d86b4b1c71
TSDilov <cv.dilov@abv.bg>
2021-09-22 10:56:48 +0300

    Added Fundamental Basic Syntax code

---
+++ Programing Fundamentals - C#/Basic Syntax, Conditional Statements and Loops/Login/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = "";
+            for (int i = username.Length - 1; i >=0; i--)
+            {
+                password += username[i];
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                string inputPas = Console.ReadLine();
+                if (inputPas == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                if (i == 3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                if (inputPas != password)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+            }
+        }
+    }
+}
