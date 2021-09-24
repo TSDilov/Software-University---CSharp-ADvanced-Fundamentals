220efafa066ebc107dcb3d5e73aee9ac12a2acc0
TSDilov <cv.dilov@abv.bg>
2021-09-24 09:47:01 +0300

    Added Fundamentals Basic Syntax code

---
+++ Programing Fundamentals - C#/Basic Syntax, Conditional Statements and Loops/Padawan Equipment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double sabrePrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+            double sabreMore = Math.Ceiling(students * 0.1);
+            int beltFree = students / 6;
+            double costs = (students + sabreMore) * sabrePrice + students * robePrice + (students - beltFree) * beltPrice;
+            if (money >= costs)
+            {
+                Console.WriteLine($"The money is enough - it would cost {costs:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {(costs - money):F2}lv more.");
+            }
+        }
+    }
+}
