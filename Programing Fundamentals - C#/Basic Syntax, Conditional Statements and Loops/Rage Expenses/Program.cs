d6b0fea25524b7f597d9ef88723fd032e7bfa62e
TSDilov <cv.dilov@abv.bg>
2021-09-24 09:48:23 +0300

    Added Fundamentals Basic Syntax code

---
+++ Programing Fundamentals - C#/Basic Syntax, Conditional Statements and Loops/Rage Expenses/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+            int trashedHeadsets = lostGames / 2;
+            int trashedMouses = lostGames / 3;
+            int trashedKeyboards = lostGames / 6;
+            int trashedDisplays;
+            if (trashedKeyboards > 1)
+            {
+                trashedDisplays = trashedKeyboards / 2;
+            }
+            else
+            {
+                trashedDisplays = 0;
+            }
+            double expenses = headsetPrice * trashedHeadsets + mousePrice * trashedMouses + keyboardPrice * trashedKeyboards + displayPrice * trashedDisplays;
+            Console.WriteLine($"Rage expenses: {expenses:F2} lv.");
+        }
+    }
+}
