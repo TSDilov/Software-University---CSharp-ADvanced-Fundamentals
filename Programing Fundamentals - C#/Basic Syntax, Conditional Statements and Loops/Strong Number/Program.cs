649f3583cbc4fe5998af96a5a17444b0635995d7
TSDilov <cv.dilov@abv.bg>
2021-09-24 09:45:22 +0300

    Added Fundamentals Basic Syntax code

---
+++ Programing Fundamentals - C#/Basic Syntax, Conditional Statements and Loops/Strong Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Strongnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int firstNum = num;
+            int sum = 0;
+            while (num > 0)
+            {
+                int currentNum = num % 10;
+                int factoriel = 1;
+                for (int i = 1; i <= currentNum; i++)
+                {
+                    factoriel *= i;
+                }
+                sum += factoriel;
+                num /= 10;
+            }
+            if (sum == firstNum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
