332dbdef9ce244a946f5f23b244cc04a8cbcca0b
TSDilov <cv.dilov@abv.bg>
2021-09-25 11:24:11 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Special Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n ; i++)
+            {
+                int num = i;
+                int sum = 0;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+                bool special = sum == 5 || sum == 7 || sum == 11;
+                
+                    Console.WriteLine($"{i} -> {special}");
+                
+            }
+        }
+    }
+}
