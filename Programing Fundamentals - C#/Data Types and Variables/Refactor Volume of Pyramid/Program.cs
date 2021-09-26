b9f09ccc3c585b2cbfe3f6807ef395674725d70c
TSDilov <cv.dilov@abv.bg>
2021-09-26 20:25:37 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RefactorVolumeofPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = length * width;
+            double vol = (area * height) / 3;
+            Console.Write("Length: ");
+            Console.Write("Width: ");
+            Console.Write("Height: ");
+            Console.Write($"Pyramid Volume: {vol:f2}");
+
+
+        }
+    }
+}
