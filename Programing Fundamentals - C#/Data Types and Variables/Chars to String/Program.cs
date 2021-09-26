ca4371187ac7a7d75a1d91ef9d1684b11841323f
TSDilov <cv.dilov@abv.bg>
2021-09-26 20:17:48 +0300

    Added Fundamentals Data types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Chars to String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CharstoString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char third = char.Parse(Console.ReadLine());
+
+            string firstString = first.ToString();
+            string secondString = second.ToString();
+            string thirdString = third.ToString();
+
+            Console.WriteLine($"{firstString}{secondString}{thirdString}");
+
+        }
+    }
+}
