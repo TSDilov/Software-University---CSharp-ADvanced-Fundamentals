d554038ec118eca662b2b7e2845c66b840cffe23
TSDilov <cv.dilov@abv.bg>
2021-09-26 20:21:15 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Lower or Upper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LowerorUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alphabet = char.Parse(Console.ReadLine());
+            if (alphabet >= 65 && alphabet <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+
+        }
+    }
+}
