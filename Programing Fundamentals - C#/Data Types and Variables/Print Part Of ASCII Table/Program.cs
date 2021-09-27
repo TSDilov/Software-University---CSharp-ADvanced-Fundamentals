0dc1637da3e57b18862d79ac432e1a55629f0760
TSDilov <cv.dilov@abv.bg>
2021-09-27 20:51:36 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrintPartOfASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndex = int.Parse(Console.ReadLine());
+            int finalIndex = int.Parse(Console.ReadLine());
+            for (int i = startIndex; i <= finalIndex; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
