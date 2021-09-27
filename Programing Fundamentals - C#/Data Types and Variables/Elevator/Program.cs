ba6d05b7492527ae5c8e51ce92cc3e26e3f3e481
TSDilov <cv.dilov@abv.bg>
2021-09-27 20:49:05 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Elevator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int cours = people / capacity;
+            if (cours * capacity == people)
+            {
+                Console.WriteLine(cours);
+            }
+            else
+            {
+                Console.WriteLine(cours + 1);
+            }
+        }
+    }
+}
