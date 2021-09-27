70fa800ad2f15208f9bd5e69c6960d117b7281c8
TSDilov <cv.dilov@abv.bg>
2021-09-27 20:50:17 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Sum of Chars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumofChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <=n; i++)
+            {
+                char alphabet = char.Parse(Console.ReadLine());
+                sum += alphabet;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
