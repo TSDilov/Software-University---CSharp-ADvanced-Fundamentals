d084fc30b4e8bbd9bd895610765f77b3e8246056
TSDilov <cv.dilov@abv.bg>
2021-09-28 21:19:23 +0300

    Added Fundamentals Data types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Water Overflow/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPours = int.Parse(Console.ReadLine());
+            int liters;
+            int sumLiters = 0;
+            for (int i = 1; i <= numberPours;  i++)
+            {
+                liters = int.Parse(Console.ReadLine());
+                sumLiters += liters;
+                if (sumLiters > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    sumLiters = sumLiters - liters;
+                }
+            }
+            Console.WriteLine(sumLiters);
+        }
+    }
+}
