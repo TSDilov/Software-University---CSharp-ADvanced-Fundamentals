61abe5da75507938c750e81d27f36c42feb6c020
TSDilov <cv.dilov@abv.bg>
2021-09-28 21:20:46 +0300

    Added Fundamentals Data types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Beer Kegs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberKegs = int.Parse(Console.ReadLine());
+            string largestKeg = "";
+            double largestVol = 0;
+            
+            for (int i = 1; i <= numberKegs; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (volume > largestVol)
+                {
+                    largestVol = volume;
+                    largestKeg = model;
+                }
+            }
+            Console.WriteLine(largestKeg);
+        }
+    }
+}
