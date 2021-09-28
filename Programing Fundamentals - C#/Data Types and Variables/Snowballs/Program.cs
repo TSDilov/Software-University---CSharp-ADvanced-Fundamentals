b8795916c39d91e44e0be63fb6f73d668b14a742
TSDilov <cv.dilov@abv.bg>
2021-09-28 21:23:16 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Snowballs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger bestValue = 0;
+            int bestSnowballSnow = 0;
+            int bestSnowballTime = 0;
+            int bestSnowballQuality = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+                BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+                if (snowballValue > bestValue)
+                {
+                    bestValue = snowballValue;
+                    bestSnowballSnow = snowballSnow;
+                    bestSnowballTime = snowballTime;
+                    bestSnowballQuality = snowballQuality;
+                }
+            }            
+            Console.WriteLine($"{bestSnowballSnow} : {bestSnowballTime} = {bestValue} ({bestSnowballQuality})");
+        }
+    }
+}
