a5a3e3994756b500a8d717dc8b813b4460c286cf
TSDilov <cv.dilov@abv.bg>
2021-09-28 21:22:04 +0300

    Added Fundamentals Data types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Spice Must Flow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int halfN = n / 2;
+            int targetsPoked = 0;
+            while (n >= m)
+            {
+                targetsPoked++;
+                n -= m;
+                if (halfN == n && y > 0)
+                {
+                    n /= y;
+                }
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(targetsPoked);
+        }
+    }
+}
