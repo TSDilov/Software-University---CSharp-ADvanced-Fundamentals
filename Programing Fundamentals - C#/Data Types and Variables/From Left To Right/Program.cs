d34e93372441a47a63f3f4c3104ffc7f2077da89
TSDilov <cv.dilov@abv.bg>
2021-09-29 10:14:53 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/From Left To Right/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace FromLefttoTheRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rotation = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotation; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                BigInteger[] numbers = { BigInteger.Parse(input[0]), BigInteger.Parse(input[1]) };
+
+                Array.Sort(numbers);
+                numbers[1] = BigInteger.Abs(numbers[1]);
+                input[1] = numbers[1].ToString();
+
+                BigInteger sum = 0;
+                foreach (var digit in input[1])
+                {
+                    sum += (BigInteger)char.GetNumericValue(digit);
+                }
+                Console.WriteLine(sum);
+
+            }
+        }
+    }
+}
