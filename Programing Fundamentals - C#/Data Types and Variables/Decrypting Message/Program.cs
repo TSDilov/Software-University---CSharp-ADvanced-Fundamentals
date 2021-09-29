d54f60f54b4e553577924b894d17f84b0e4830f1
TSDilov <cv.dilov@abv.bg>
2021-09-29 10:18:55 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Decrypting Message/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace DecryptingMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int numberOFCharacters = int.Parse(Console.ReadLine());
+            String message = string.Empty;
+            for (int i = 0; i < numberOFCharacters; i++)
+            {
+                char character = char.Parse(Console.ReadLine());
+                char newChar = (char)(character + key);
+                message += newChar;
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
