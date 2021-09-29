7ff77cfe4545c6574c9fd8430b7a74f7683f655c
TSDilov <cv.dilov@abv.bg>
2021-09-29 10:16:11 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Floating Equality/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FloatingEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal floatOne = decimal.Parse(Console.ReadLine());
+            decimal floatTwo = decimal.Parse(Console.ReadLine());
+
+            decimal eps = 0.000001M;
+            decimal difference = Math.Abs(floatOne - floatTwo);
+
+            if (difference < eps)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
