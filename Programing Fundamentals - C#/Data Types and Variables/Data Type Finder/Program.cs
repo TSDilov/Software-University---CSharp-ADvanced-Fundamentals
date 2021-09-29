589d1b11400ce7b3b74a6d0aac136cfcf5ef540e
TSDilov <cv.dilov@abv.bg>
2021-09-29 10:13:26 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Data Type Finder/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DataTypeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool notType = true;
+            while (notType)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    notType = false;
+                    break;
+                }
+                bool isInt = int.TryParse(input, out int integer);
+                bool isFloat = float.TryParse(input, out float floating);
+                bool isChar = char.TryParse(input, out char character);
+                bool isBool = bool.TryParse(input, out bool boolean);
+                string type = string.Empty;
+
+                if (isInt)
+                {
+                    type = "integer";
+                }
+                else if (isFloat)
+                {
+                    type = "floating point";
+                }
+                else if (isChar)
+                {
+                    type = "character";
+                }
+                else if (isBool)
+                {
+                    type = "boolean";
+                }
+                else
+                {
+                    type = "string";
+                }
+                Console.WriteLine($"{input} is {type} type");
+            }
+        }
+    }
+}
