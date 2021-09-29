590b00e4843c8f9fa3809d3bcf9785e57f62b9ea
TSDilov <cv.dilov@abv.bg>
2021-09-29 10:20:03 +0300

    Added Fundamentals Data Types code

---
+++ Programing Fundamentals - C#/Data Types and Variables/Balanced Brackets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int countOpenBrackets = 0;
+            int countCloseBrackets = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (input == "(")
+                {
+                    countOpenBrackets++;
+                }
+                else if (input == ")")
+                {
+                    countCloseBrackets++;
+                    if (countOpenBrackets - countCloseBrackets !=0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine(countOpenBrackets == countCloseBrackets ? "BALANCED" : "UNBALANCED");
+        }
+    }
+}
