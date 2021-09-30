aa67d7910fc91d4f238a7d7b338b94cbbbeb0e40
TSDilov <cv.dilov@abv.bg>
2021-09-30 22:38:30 +0300

    Added Fundamentals Arrays code

---
+++ Programing Fundamentals - C#/Arrays/Rounding Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split()
+                .Select(double.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    numbers[i] = 0;
+                }
+
+                int roundedNumber = (int) Math.Round(numbers[i], 
+                    MidpointRounding.AwayFromZero);
+
+                Console.WriteLine($"{numbers[i]} => {roundedNumber}");
+            }
+
+        }
+    }
+}
