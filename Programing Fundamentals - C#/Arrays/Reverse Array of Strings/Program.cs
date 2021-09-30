b74a9ac065fd0e5ef87297d27d6cc83ce90d7ef4
TSDilov <cv.dilov@abv.bg>
2021-09-30 22:39:36 +0300

    Added Fundamentals Arrays code

---
+++ Programing Fundamentals - C#/Arrays/Reverse Array of Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArrayofStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Reverse().ToArray();
+
+            Console.WriteLine(string.Join(" ", input));
+
+        }
+    }
+}
