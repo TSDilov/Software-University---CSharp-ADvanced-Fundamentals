354ab3afb62dd3090f696721dc6fbed58be424cb
TSDilov <cv.dilov@abv.bg>
2021-10-03 11:39:38 +0300

    Added Fundamentals Arrays code

---
+++ Programing Fundamentals - C#/Arrays/Sum Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
