4485e7d6a8b019ef788747fd721225ab5c66aa54
TSDilov <cv.dilov@abv.bg>
2021-10-03 11:43:15 +0300

    Added Fundamentals Arrays code

---
+++ Programing Fundamentals - C#/Arrays/Condense Array to Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace CondenseArraytoNumber_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split()
+                .Select(int.Parse).ToArray();
+            int n = nums.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int[] condenced = new int[nums.Length - 1];
+                for (int j = 0; j < condenced.Length; j++)
+                {
+                    condenced[j] = nums[j] + nums[j + 1];
+                }
+                nums = condenced;
+            }
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
