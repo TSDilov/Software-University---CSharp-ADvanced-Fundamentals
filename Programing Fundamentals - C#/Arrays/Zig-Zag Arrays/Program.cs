e24d0f388eb8b1d65b6e68dcd57963ae676db932
TSDilov <cv.dilov@abv.bg>
2021-10-04 19:58:17 +0300

    Added Fundamentals Arrays code

---
+++ Programing Fundamentals - C#/Arrays/Zig-Zag Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Zig_ZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfInt = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[numOfInt];
+            int[] arr2 = new int[numOfInt];
+            
+            for (int i = 0; i < numOfInt; i++)
+            {
+                int[] integers = Console.ReadLine().Split().
+                    Select(int.Parse).ToArray();
+                int firstNumber = integers[0];
+                int secondNumber = integers[1];
+                if (i % 2 == 0)
+                {
+                    arr1[i] = firstNumber;
+                    arr2[i] = secondNumber;
+                }
+                else
+                {
+                    arr1[i] = secondNumber;
+                    arr2[i] = firstNumber;
+                }
+            }
+            Console.WriteLine(String.Join(" ", arr1));
+            Console.WriteLine(String.Join(" ", arr2));
+        }
+    }
+}
