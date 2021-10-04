792a86ec6c4343c5424925b73f56720cf736cc43
TSDilov <cv.dilov@abv.bg>
2021-10-04 19:59:37 +0300

    Added Fundamentals Arrays code

---
+++ Programing Fundamentals - C#/Arrays/Array Rotation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfIntegers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int numberOfRotations = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfRotations % arrayOfIntegers.Length; i++)
+            {
+                int firstNumber = arrayOfIntegers[0];
+                for (int j = 0; j < arrayOfIntegers.Length - 1; j++)
+                {
+                    arrayOfIntegers[j] = arrayOfIntegers[j + 1];
+                }
+                arrayOfIntegers[arrayOfIntegers.Length - 1] = firstNumber;
+            }
+            Console.WriteLine(String.Join(" ", arrayOfIntegers));
+        }
+    }
+}
