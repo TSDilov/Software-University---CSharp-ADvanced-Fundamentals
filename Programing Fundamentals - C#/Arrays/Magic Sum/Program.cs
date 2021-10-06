01f2f6a812f8e0b5e8517ba8029e5f707cefff63
TSDilov <cv.dilov@abv.bg>
2021-10-06 10:40:13 +0300

    Added Fundamentals Arrays code

---
+++ Programing Fundamentals - C#/Arrays/Magic Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumOfTwo = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (j != i &&numbers[i] + numbers [j] == sumOfTwo)
+                    {
+                        Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
