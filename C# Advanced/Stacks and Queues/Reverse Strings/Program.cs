76c137ab5b1f402117b167aec0153f93a80d26d1
TSDilov <cv.dilov@abv.bg>
2021-10-07 21:04:41 +0300

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseStringsWithStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> forReverse = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                forReverse.Push(ch);
+            }
+            while (forReverse.Count > 0)
+            {
+                Console.Write(forReverse.Pop());
+            }
+        }
+    }
+}
