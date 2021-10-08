56b26de5c7b0a0e690a31f62d7ff43c5375d5bce
TSDilov <cv.dilov@abv.bg>
2021-10-08 20:59:04 +0300

    Added C# Advanced Defining Classes code

---
+++ C# Advanced/Defining Classes/Speed Racing/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKm { get; set; }
+        public double TravelledDistance { get; set; } = 0;
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKm)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKm = fuelConsumptionPerKm;
+        }
+        public void Drive(double distance)
+        {
+            double fuelConsumed = distance * FuelConsumptionPerKm;
+            if (FuelAmount - fuelConsumed >= 0)
+            {
+                FuelAmount -= fuelConsumed;
+                TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:F2} {TravelledDistance}";
+        }
+    }
+}
