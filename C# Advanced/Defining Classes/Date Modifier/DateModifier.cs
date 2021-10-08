a3a557b6060b74d5ed9783f002d4fa3900b8da56
TSDilov <cv.dilov@abv.bg>
2021-10-08 20:58:05 +0300

    Added C# Advanced Defining Classes code

---
+++ C# Advanced/Defining Classes/Date Modifier/DateModifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public static class DateModifier
+    {
+        public static int GetDifferenceOfDates(string firstDateAsString, string secondDateAsString) 
+        {
+            var firstDate = DateTime.Parse(firstDateAsString);
+            var secondDate = DateTime.Parse(secondDateAsString);
+            int difference = (firstDate - secondDate).Days;
+            if (difference > 0)
+            {
+                return (firstDate - secondDate).Days;
+            }
+            if (difference < 0)
+            {
+                return (secondDate - firstDate).Days;
+            }
+            return 0;
+        }
+    }
+}
