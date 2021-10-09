f80f8c216f3fb01bc41b1eaa56e7dc563d8c443a
TSDilov <cv.dilov@abv.bg>
2021-10-09 10:22:25 +0300

    Added C# Advanced Defining Classes code

---
+++ C# Advanced/Defining Classes/Car Salesman/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Engine
+    {
+        public string Model { get; set; }
+        public string Power { get; set; }
+        public string Displacement { get; set; } = "n/a";
+        public string Efficiency { get; set; } = "n/a";
+        public Engine(string model, string power)
+        {
+            Model = model;
+            Power = power;
+        }
+        public override string ToString()
+        {
+            return $"{Model}:\n    Power: {Power}\n    Displacement: {Displacement}\n    Efficiency: {Efficiency}";
+        }
+    }
+}
