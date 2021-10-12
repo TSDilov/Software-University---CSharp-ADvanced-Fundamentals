a3d58ce76bd5eb5ccfea53a8e4235d8cb255da94
TSDilov <cv.dilov@abv.bg>
2021-10-12 22:57:37 +0300

    Added C# Fundamentals Arrays code

---
+++ Programing Fundamentals - C#/Arrays/Recursive Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            arrayFibonacci = new int[n + 1];
+            GetFibonacci(n);
+
+            Console.WriteLine(arrayFibonacci[n]);
+
+        }
+        private static int[] arrayFibonacci = null;
+        static int GetFibonacci(int index)
+        {
+            if (arrayFibonacci[index] != 0)
+            {
+                return arrayFibonacci[index];
+            }
+            if (index <= 2)
+            {
+                arrayFibonacci[index] = 1;
+                return arrayFibonacci[index];
+            }
+            arrayFibonacci[index] = GetFibonacci(index - 1) + GetFibonacci(index - 2);
+
+            return arrayFibonacci[index];
+        }
+    }
+}
