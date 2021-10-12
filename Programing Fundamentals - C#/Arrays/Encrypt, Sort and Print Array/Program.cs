af0d60057d8c44342e08ad9e1e2a57f077cee447
TSDilov <cv.dilov@abv.bg>
2021-10-12 22:45:10 +0300

    Added C# Fundamentals Arrays code

---
+++ Programing Fundamentals - C#/Arrays/Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Encrypt_SortandPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] sums = new int[n];  
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                int sum = 0;
+
+                foreach (char letter in input)
+                {
+                    if (letter == 'a' || letter == 'A' || letter == 'e' || letter == 'E' || letter == 'o' ||
+                        letter == 'O' || letter == 'i' || letter == 'I' || letter == 'u' || letter == 'U')
+                        
+                    {
+                        sum += (int)letter * input.Length;
+                    }
+                    else
+                    {
+                        sum += (int)letter / input.Length;
+                    }
+                }
+                sums[i] = sum;
+            }
+            Array.Sort(sums);
+            foreach (var item in sums)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
