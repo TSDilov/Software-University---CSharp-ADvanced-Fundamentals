00a798966234ad3db8d9c7688578b398bcaf99c8
TSDilov <cv.dilov@abv.bg>
2021-10-17 10:45:18 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Calculations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = Console.ReadLine();
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            if (operation == "add")
+            {
+                AddMethod(firstNumber, secondNumber);
+            }
+            else if (operation == "subtract")
+            {
+                SubtractMethod(firstNumber, secondNumber);
+            }
+            else if (operation == "multiply")
+            {
+                MultiplyMethod(firstNumber, secondNumber);
+            }
+            else
+            {
+                DevideMethod(firstNumber, secondNumber);
+            }
+                
+        }
+
+        static void DevideMethod(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber / secondNumber;
+            Console.WriteLine($"{result}");
+        }
+
+        static void MultiplyMethod(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber * secondNumber;
+            Console.WriteLine($"{result}");
+        }
+
+        static void SubtractMethod(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber - secondNumber;
+            Console.WriteLine($"{result}");
+        }
+
+        static void AddMethod(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber + secondNumber;
+            Console.WriteLine($"{result}");
+        }
+    }
+}
