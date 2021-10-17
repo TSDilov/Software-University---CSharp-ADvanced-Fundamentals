0a59ef3b719167c4e1361c58e72940df85ef01bf
TSDilov <cv.dilov@abv.bg>
2021-10-17 10:37:30 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SignofIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            MethodwithNumbers(inputNumber);
+        }
+        static void MethodwithNumbers(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number > 0 )
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else
+            {
+                Console.WriteLine("The number 0 is zero.");
+            }
+        }
+    }
+}
