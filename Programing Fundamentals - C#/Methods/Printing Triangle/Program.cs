d71897bd2bb131842fe685570430cc03dc639f91
TSDilov <cv.dilov@abv.bg>
2021-10-17 10:47:03 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Printing Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintingTriangle(n);
+        }
+
+        static void PrintingTriangle(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                Row(row);
+            }
+            for (int row = n - 1; row >= 1; row--)
+            {
+                Row(row);
+            }
+        }
+
+        private static void Row(int row)
+        {
+            for (int j = 1; j <= row; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
