06fdbed608de450dd298ddc51d9d541ab2711b27
TSDilov <cv.dilov@abv.bg>
2021-10-18 10:25:57 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Multiply Evens by Odds/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MultiplyEvensbyOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            input = Math.Abs(input);
+
+            int evenSum = EvenSumMethod(input);
+            int oddSum = OddSumMethod(input);
+            Console.WriteLine(MultiplyMetho(evenSum, oddSum));
+        }
+
+        static int MultiplyMetho(int evenSum, int oddSum)
+        {
+            int result = evenSum * oddSum;
+            return result;
+        }
+
+        static int OddSumMethod(int input)
+        {
+            string inputArray = input.ToString();
+            int sumOdd = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                char digit = inputArray[i];
+                int numberFromInputArray = (int)char.GetNumericValue(digit);
+                if (numberFromInputArray % 2 != 0)
+                {
+                    sumOdd += numberFromInputArray;
+                }
+            }
+            return sumOdd;
+        }
+
+        static int EvenSumMethod(int input)
+        {
+            string inputArray = input.ToString();
+            int sumEven = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                char digit = inputArray[i];
+                int numberFromInputArray = (int)char.GetNumericValue(digit);
+                if (numberFromInputArray % 2 == 0)
+                {
+                    sumEven += numberFromInputArray;
+                }
+            }
+            return sumEven;
+        }
+    }
+}
