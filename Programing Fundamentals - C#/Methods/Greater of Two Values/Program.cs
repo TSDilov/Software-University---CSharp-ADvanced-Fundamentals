d0ad2febcfc6013701cef11cf5d3980f3909abd8
TSDilov <cv.dilov@abv.bg>
2021-10-18 10:24:29 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Greater of Two Values/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace GreaterofTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            if (type == "int")
+            {
+                int firstInt = int.Parse(Console.ReadLine());
+                int secondInt = int.Parse(Console.ReadLine());
+                Console.WriteLine(MethodGetMax(firstInt, secondInt));
+            }
+            else if (type == "char")
+            {
+                char firstChar = char.Parse(Console.ReadLine());
+                char secondChar = char.Parse(Console.ReadLine());
+                Console.WriteLine((char)MethodGetMax(firstChar, secondChar));
+            }
+            else if (type == "string")
+            {
+                string firstString = Console.ReadLine();
+                string secondString = Console.ReadLine();
+                Console.WriteLine(MethodGetMax(firstString, secondString));
+            }
+        }
+
+        static string MethodGetMax(string firstString, string secondString)
+        {
+            int result = firstString.CompareTo(secondString);
+
+            int firstStringSum = MethodGetSumFromString(firstString);
+            int secondStringSum = MethodGetSumFromString(secondString);
+
+            if (result > 0 )
+            {
+                return firstString;
+            }
+            return secondString;
+
+        }
+
+        private static int MethodGetSumFromString(string valueString)
+        {
+            int stringSum = 0;
+            for (int i = 0; i < valueString.Length; i++)
+            {
+                stringSum += valueString[i];
+            }
+
+            return stringSum;
+        }
+
+        static int MethodGetMax(int firstInt, int secondInt)
+        {
+            if (firstInt > secondInt)
+            {
+                return firstInt;
+            }
+            return secondInt;
+        }
+    }
+}
