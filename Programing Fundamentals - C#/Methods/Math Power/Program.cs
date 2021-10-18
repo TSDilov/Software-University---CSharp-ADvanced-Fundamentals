0e8615c6554ff626299b2317389af7cb39b7f621
TSDilov <cv.dilov@abv.bg>
2021-10-18 10:23:13 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Math Power/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            int givenPower = int.Parse(Console.ReadLine());
+
+            double result = MethodRaisePower(num, givenPower);
+            Console.WriteLine(result);
+        }
+
+        static double MethodRaisePower(double num, int givenPower)
+        {
+            double result = 1.0;
+            for (int i = 0; i < givenPower; i++)
+            {
+                result *= num;
+            }
+            return (double)result;
+        }
+    }
+}
