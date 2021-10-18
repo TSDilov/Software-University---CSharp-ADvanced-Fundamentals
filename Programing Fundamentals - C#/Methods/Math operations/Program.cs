dc564862ed0b3bd264dc40025ef10c7d17cc7ea1
TSDilov <cv.dilov@abv.bg>
2021-10-18 10:27:10 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Math operations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Mathoperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            char operatorr = char.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            double result = CalculateMethod(firstNumber, operatorr, secondNumber);
+            Console.WriteLine(result);
+        }
+
+        static double CalculateMethod(double firstNumber, char operatorr, double secondNumber)
+        {
+            double result = 0;
+            if (operatorr == '+')
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if (operatorr == '-')
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if (operatorr == '*')
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if (operatorr == '/')
+            {
+                result = firstNumber / secondNumber;
+            }
+            return result;
+        }
+    }
+}
