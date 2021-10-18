1a512cde89bf36adbf27743d0e2176b39398c08d
TSDilov <cv.dilov@abv.bg>
2021-10-18 10:21:48 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Repeat String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int repeatsNum = int.Parse(Console.ReadLine());
+            string result = MethodRepeat(text, repeatsNum);
+            Console.WriteLine(result);
+        }
+
+        static string MethodRepeat(string text, int repeatsNum)
+        {
+            string result = "";
+
+            for (int i = 0; i < repeatsNum; i++)
+            {
+                result += text;
+            }
+            return result;
+        }
+    }
+}
