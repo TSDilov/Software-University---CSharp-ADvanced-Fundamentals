d2b258c90e898a5c9d60b4548add2e22f070ccfd
TSDilov <cv.dilov@abv.bg>
2021-10-18 10:19:59 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Calculating Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double area = MethodArea(height,width);
+            Console.WriteLine(area);
+        }
+
+        static double MethodArea(double height, double width)
+        {
+            return height * width;
+        }
+    }
+}
