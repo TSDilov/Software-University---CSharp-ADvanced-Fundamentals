61cf6a2e62dba674ce7c3c391281755f870f8f68
TSDilov <cv.dilov@abv.bg>
2021-10-19 09:35:12 +0300

    Added C# Advanced Implementing LinkedList code

---
+++ C# Advanced/Implementing LinkedList/DoublyLinkedList.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementingLinkedList
+{
+    public class DoublyLinkedList<T>
+    {
+        private LinkedListItem<T> first = null;
+        private LinkedListItem<T> last = null;
+        public int Count
+        {
+            get
+            {
+                int count = 0;
+
+                LinkedListItem<T> current = first;
+                while (current != null)
+                {
+                    count++;
+                    current = current.Next;
+                }
+
+                return count;
+            }
+        }
+        public void AddFirst(T element)
+        {
+            LinkedListItem<T> newItem = new LinkedListItem<T>(element);
+            if (first == null)
+            {
+                first = newItem;
+                last = newItem;
+            }
+            else
+            {
+                newItem.Next = first;
+                first.Previous = newItem;
+                first = newItem;
+            }
+        }
+        public void DeleteByValue(T value)
+        {
+            var current = first;
+            while (current != null)
+            {
+                if (current.Value.Equals(value))
+                {
+                    if (current == first)
+                    {
+                        RemoveFirst();
+                        break;
+                    }
+                    if (current == last)
+                    {
+                        RemoveLast();
+                        break;
+                    }
+                        current.Next.Previous = current.Previous;
+                        current.Previous.Next = current.Next;
+                        break;
+                }
+
+                current = current.Next;
+            }
+        }
+        public void AddLast(T element)
+        {
+            LinkedListItem<T> newItem = new LinkedListItem<T>(element);
+            if (last == null)
+            {
+                first = newItem;
+                last = newItem;
+            }
+            else
+            {
+                last.Next = newItem;
+                newItem.Previous = last;
+                last = newItem;
+            }
+        }
+        public T RemoveFirst()
+        {
+            if (first == null) 
+            {
+                throw new InvalidOperationException("Linked list empty!");
+            }
+
+            T currentFirstValue = first.Value;
+            if (first == last) 
+            {
+                first = null;
+                last = null;
+            }
+            else 
+            {
+                LinkedListItem<T> newFirst = first.Next;
+                newFirst.Previous = null;
+                first = newFirst;
+            }
+
+            return currentFirstValue;
+        }
+        public T RemoveLast()
+        {
+            if (last == null)
+            {
+                throw new InvalidOperationException("Linked list empty!");
+            }
+
+            T currentLastValue = last.Value;
+            if (first == last)
+            {
+                first = null;
+                last = null;
+            }
+            else
+            {
+                LinkedListItem<T> newLast = last.Previous;
+                newLast.Next = null;
+                last = newLast;
+            }
+
+            return currentLastValue;
+        }
+        public void ForEach(Action<T> action)
+        {
+            LinkedListItem<T> current = first;
+            while (current != null)
+            {
+                action(current.Value);
+                current = current.Next;
+            }
+        }
+        public T[] ToArray()
+        {
+            T[] array = new T[Count];
+
+            LinkedListItem<T> current = first;
+            int index = 0;
+            while (current != null)
+            {
+                array[index] = current.Value;
+                index++;
+                current = current.Next;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ C# Advanced/Implementing LinkedList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ImplementingLinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var stack = new DoublyLinkedList<int>();
+                stack.RemoveFirst();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            var listOfStrings = new DoublyLinkedList<string>();
+            listOfStrings.AddFirst("Ceco");
+            Console.WriteLine(string.Join("-", listOfStrings.ToArray()));
+            listOfStrings.AddFirst("Georgi");
+            Console.WriteLine(string.Join("-", listOfStrings.ToArray()));
+            listOfStrings.AddLast("Ivan");
+            Console.WriteLine(string.Join("-", listOfStrings.ToArray()));
+
+            var list = new DoublyLinkedList<int>();
+            list.AddFirst(3);
+            Console.WriteLine(string.Join("-", list.ToArray()));
+
+            list.AddFirst(2);
+            Console.WriteLine(string.Join("-", list.ToArray()));
+
+            list.AddFirst(1);
+            Console.WriteLine(string.Join("-", list.ToArray()));
+
+            list.AddLast(4);
+            Console.WriteLine(string.Join("-", list.ToArray()));
+
+            list.AddFirst(0);
+            Console.WriteLine(string.Join("-", list.ToArray()));
+
+            list.AddLast(100);
+            Console.WriteLine(string.Join("-", list.ToArray()));
+
+            list.RemoveFirst();
+            Console.WriteLine(string.Join("-", list.ToArray()));
+
+            list.RemoveLast();
+            Console.WriteLine(string.Join("-", list.ToArray()));
+
+            list.AddLast(5);
+            Console.WriteLine(string.Join("-", list.ToArray()));
+            list.DeleteByValue(1);
+            Console.WriteLine(string.Join("-", list.ToArray()));
+
+            list.ForEach(x => Console.WriteLine("--" + x));
+        }
+    }
+}
