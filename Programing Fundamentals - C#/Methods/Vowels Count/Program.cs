42a45bf0fa022f5c85d3f20307d153facddaf922
TSDilov <cv.dilov@abv.bg>
2021-10-20 10:08:07 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Vowels Count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            MethodNumberOFVowelsInWord(word);
+        }
+
+        static void MethodNumberOFVowelsInWord(string word)
+        {
+            int count = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char letter = word[i];
+                if (letter == 'a'  || letter == 'e' ||  letter == 'i' 
+                    || letter == 'o' ||  letter == 'u'  || letter == 'y')
+                {
+                    count += 1;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
