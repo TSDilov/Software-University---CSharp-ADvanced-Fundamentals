b3dd56b8b64017b120a9f89d5dd228a42897792c
TSDilov <cv.dilov@abv.bg>
2021-10-20 10:03:50 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Smallest of Three Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SmallestofThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int minNumber = MethodForMinNumber(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(minNumber);
+        }
+
+        static int MethodForMinNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int minNumber = firstNumber;
+            if (secondNumber < firstNumber)
+            {
+                minNumber = secondNumber;
+            }
+            if (thirdNumber < minNumber)
+            {
+                minNumber = thirdNumber;
+            }
+            return minNumber;
+        }
+    }
+}
