b9235c3b453d6514c39d0c1949dc24cd5a3108e6
TSDilov <cv.dilov@abv.bg>
2021-10-21 09:47:06 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/NxN Matrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            MethodForMatrix(n);
+        }
+
+        static void MethodForMatrix(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
