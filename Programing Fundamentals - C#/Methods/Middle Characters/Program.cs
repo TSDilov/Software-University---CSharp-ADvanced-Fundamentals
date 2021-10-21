129ae15fc6ce7037c8b6fa91d5b509246409f0e9
TSDilov <cv.dilov@abv.bg>
2021-10-21 09:44:59 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Middle Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            PrintMiddleChars(text);
+        }
+
+        static void PrintMiddleChars(string text)
+        {
+            if (text.Length % 2 == 0)
+            {
+                Console.WriteLine($"{text[text.Length / 2 - 1]}{text[text.Length / 2]}");
+            }
+            else
+            {
+                Console.WriteLine(text[text.Length / 2]);
+            }
+        }
+    }
+}
