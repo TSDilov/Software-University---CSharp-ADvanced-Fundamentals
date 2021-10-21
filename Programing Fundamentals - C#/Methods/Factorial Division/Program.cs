6d7509e801c5f49d1b960bba7930bfdb6dc91466
TSDilov <cv.dilov@abv.bg>
2021-10-21 09:49:17 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Factorial Division/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            double result = MethodFaktorialFirstNumber(firstNumber) / MethodFaktorialSecondNumber(secondNumber);
+            Console.WriteLine($"{result:F2}");
+        }
+
+        static double MethodFaktorialSecondNumber(int secondNumber)
+        {
+            double faktorial = 1;
+            for (int j = 1; j <= secondNumber ; j++)
+            {
+                faktorial *= j;
+            }
+            return faktorial;
+        }
+
+        static double MethodFaktorialFirstNumber(int firstNumber)
+        {
+            double faktorial = 1;
+            for (int i = 1; i <= firstNumber; i++)
+            {
+                faktorial *= i;
+            }
+            return faktorial;
+        }
+    }
+}
