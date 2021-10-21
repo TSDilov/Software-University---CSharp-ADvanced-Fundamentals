50f069184d2d791b0dbb7b378de0d4b9e239c375
TSDilov <cv.dilov@abv.bg>
2021-10-21 09:42:13 +0300

    Added C# Fundamentals Method code

---
+++ Programing Fundamentals - C#/Methods/Add and Subtract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AddandSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int result = MethodSubstractFromSum(MethodSum(firstNumber, secondNumber), thirdNumber);
+            Console.WriteLine(result);
+        }
+
+        static int MethodSubstractFromSum(int sum, int thirdNumber)
+        {
+            return sum - thirdNumber;
+        }
+
+        static int MethodSum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
