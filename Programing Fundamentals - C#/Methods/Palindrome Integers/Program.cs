cdbbd2d3fbcb1956c7c7236cd17fc95e42d59138
TSDilov <cv.dilov@abv.bg>
2021-10-21 09:51:15 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Palindrome Integers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                bool isPalidrome = MethodForPalidrome(input);
+                Console.WriteLine(isPalidrome);
+                input = Console.ReadLine(); 
+            }
+        }
+
+        static bool MethodForPalidrome(string input)
+        {
+            string reverseInput = string.Empty;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reverseInput += input[i];
+            }
+            bool isPalidrome = true;
+            if (input == reverseInput)
+            {
+                isPalidrome = true;
+            }
+            else
+            {
+                isPalidrome = false;
+            }
+            return isPalidrome;
+        }
+    }
+}
