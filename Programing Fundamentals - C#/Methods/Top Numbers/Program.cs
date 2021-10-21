8f1418c34505d72bdaae87d6fb6a8d357bee9d9f
TSDilov <cv.dilov@abv.bg>
2021-10-21 09:52:59 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Top Numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <=n; i++)
+            {
+                if (MethodSumDevideByEight(i) && MethodAtLeastOneOddDIgit(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static bool MethodAtLeastOneOddDIgit(int number)
+        {
+            while (number > 0)
+            {
+                int currentDigit = number % 10;
+                if (currentDigit % 2 == 1)
+                {
+                    return true;
+                }
+                number /= 10;
+            }
+            return false;
+        }
+
+        static bool MethodSumDevideByEight(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            bool isDevisibleByEight = sum % 8 == 0;
+            return isDevisibleByEight;
+        }
+    }
+}
