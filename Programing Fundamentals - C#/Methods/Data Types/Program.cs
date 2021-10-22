6aa45cb3a11e04dc7a927bfe4141a6ee9629b40c
TSDilov <cv.dilov@abv.bg>
2021-10-22 20:29:45 +0300

    Added C# Fundamentals Methods code

---
+++ Programing Fundamentals - C#/Methods/Data Types/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string final = string.Empty;
+
+            switch (input)
+            {
+                case "int":
+                    int intInput = int.Parse(Console.ReadLine());
+                    final = InputOperation(intInput);
+                    break;
+                case "real":
+                    double doubleIput = double.Parse(Console.ReadLine());
+                    final = $"{InputOperation(doubleIput):F2}";
+                    break;
+                case "string":
+                    string stringInput = Console.ReadLine();
+                    final = InputOperation(stringInput);
+                    break;
+            }
+
+            Console.WriteLine(final);
+        }
+        static string InputOperation(int input) 
+        {
+            return (input * 2).ToString();
+        }
+        static double InputOperation(double input) 
+        {
+            return input * 1.5;
+        }
+        static string InputOperation(string input) 
+        {
+            return $"${input}$";
+        }
+    }
+}
