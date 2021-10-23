ab1aca00dbdf27127edaa36ac7481bd02e95b451
TSDilov <cv.dilov@abv.bg>
2021-10-23 08:50:36 +0300

    Added C# Fundamentals Lists code

---
+++ Programing Fundamentals - C#/Lists/Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveNegativesandReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int number = numbers[i];
+                numbers.RemoveAll(number => number < 0);
+            }
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                numbers.Reverse();
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
