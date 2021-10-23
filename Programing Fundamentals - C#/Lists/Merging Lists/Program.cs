906f83ada79a91c2c3444c3088f8b957d0923f6a
TSDilov <cv.dilov@abv.bg>
2021-10-23 08:48:04 +0300

    Added C# Fundamentals Lists code

---
+++ Programing Fundamentals - C#/Lists/Merging Lists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            List<int> secondNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+            int maxCount = Math.Max(firstNumbers.Count, secondNumbers.Count);
+            for (int i = 0; i < maxCount; i++)
+            {
+                if (i < firstNumbers.Count)
+                {
+                    result.Add(firstNumbers[i]);
+                }
+                if (i < secondNumbers.Count)
+                {
+                    result.Add(secondNumbers[i]);
+                }
+            }
+                       
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
