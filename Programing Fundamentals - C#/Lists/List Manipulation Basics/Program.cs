e6c186321599a9789a4e6f3fa3ab79ef87728bda
TSDilov <cv.dilov@abv.bg>
2021-10-23 08:51:50 +0300

    Added C# Fundamentals Lists code

---
+++ Programing Fundamentals - C#/Lists/List Manipulation Basics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+                
+                string[] commands = command.Split();
+                
+                if (commands[0] == "Add")
+                {
+                    numbers.Add(int.Parse(commands[1]));
+                }
+                else if (commands[0] == "Remove")
+                {                    
+                    numbers.Remove(int.Parse(commands[1]));
+                }
+                else if (commands[0] == "RemoveAt")
+                {
+                    numbers.RemoveAt(int.Parse(commands[1]));
+                }
+                else if (commands[0] == "Insert")
+                {
+                    numbers.Insert(int.Parse(commands[2]), int.Parse(commands[1]));
+                }                
+            }
+            
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
