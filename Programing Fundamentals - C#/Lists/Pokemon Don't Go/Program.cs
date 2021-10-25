e0c55c97d9e4ceb8de2f5a1c9b8680bc84bc184f
TSDilov <cv.dilov@abv.bg>
2021-10-25 18:50:30 +0300

    Added C# Fundamentals Lists code

---
+++ Programing Fundamentals - C#/Lists/Pokemon Don't Go/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonDon_tGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int sum = 0;
+
+            while (integers.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+                if (index < 0)
+                {
+                    int removePokemon = integers[0];
+                    sum += removePokemon;
+                    integers[0] = integers[integers.Count - 1];
+                    for (int i = 0; i < integers.Count; i++)
+                    {
+                        if (integers[i] <= removePokemon)
+                        {
+                            integers[i] += removePokemon;
+                        }
+                        else
+                        {
+                            integers[i] -= removePokemon;
+                        }
+                    }
+                }
+                else if (index > integers.Count - 1)
+                {
+                    int removePokemon = integers[integers.Count - 1];
+                    sum += removePokemon;
+                    integers[integers.Count - 1] = integers[0];
+                    for (int i = 0; i < integers.Count; i++)
+                    {
+                        if (integers[i] <= removePokemon)
+                        {
+                            integers[i] += removePokemon;
+                        }
+                        else
+                        {
+                            integers[i] -= removePokemon;
+                        }
+                    }
+                }
+                else
+                {
+                    sum += integers[index];
+                    int removePokemon = integers[index];
+                    integers.RemoveAt(index);
+
+                    for (int i = 0; i < integers.Count; i++)
+                    {
+                        if (integers[i] <= removePokemon)
+                        {
+                            integers[i] += removePokemon;
+                        }
+                        else
+                        {
+                            integers[i] -= removePokemon;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
