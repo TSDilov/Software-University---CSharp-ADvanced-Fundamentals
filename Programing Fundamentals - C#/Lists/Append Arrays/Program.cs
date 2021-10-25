0366016a1d5ae67ff7c506a1e4765a35bed566bc
TSDilov <cv.dilov@abv.bg>
2021-10-25 18:48:02 +0300

    Added C# Fundamentals Lists code

---
+++ Programing Fundamentals - C#/Lists/Append Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> entryList = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<int> endList = new List<int>();
+            for (int i = entryList.Count - 1; i >= 0; i--)
+            {
+                int[] numbers = entryList[i].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+                foreach (var number in numbers)
+                {
+                    endList.Add(number);
+                }
+            }
+            Console.WriteLine(string.Join(" ", endList));
+        }
+    }
+}
