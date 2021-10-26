5bde3fe667fb0ea5ac07e6f7c475dd53557d3f7d
TSDilov <cv.dilov@abv.bg>
2021-10-26 11:23:50 +0300

    Added C# Fundamentals Lists code

---
+++ Programing Fundamentals - C#/Lists/Mixed up Lists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MixedupLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+
+            while (firstNumbers.Count > 0 && secondNumbers.Count > 0)
+            {
+                result.Add(firstNumbers[0]);
+                firstNumbers.RemoveAt(0);
+                result.Add(secondNumbers[secondNumbers.Count - 1]);
+                secondNumbers.RemoveAt(secondNumbers.Count - 1);
+            }
+
+            if (firstNumbers.Count > 0)
+            {
+                firstNumbers.Sort();
+                result.Sort();
+                Console.WriteLine(string.Join(" ", result.Where(n => n > firstNumbers[0] && n < firstNumbers[1])));
+            }
+            else
+            {
+                secondNumbers.Sort();
+                result.Sort();
+                Console.WriteLine(string.Join(" ", result.Where(n => n > secondNumbers[0] && n < secondNumbers[1])));
+            }
+        }
+    }
+}
