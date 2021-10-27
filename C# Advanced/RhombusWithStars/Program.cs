4806e0fc546bf5ae0cd0788545bc119ad9a25d34
TSDilov <cv.dilov@abv.bg>
2021-10-27 14:27:40 +0300

    Added C# Advanced Personal Rhombus Task

---
+++ C# Advanced/RhombusWithStars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace RombWithStars
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var rhombusDrawer = new RhombusAsStringDrawer();
+            var rhombusAsString = rhombusDrawer.Draw(n);
+            Console.WriteLine(rhombusAsString);
+
+        }
+        
+    }
+}
---
+++ C# Advanced/RhombusWithStars/RhombusAsStringDrawer.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Text;
+
+namespace RombWithStars
+{
+    public class RhombusAsStringDrawer
+    {
+        public string Draw(int sizeOfRomb)
+        {
+            StringBuilder sb = new StringBuilder();
+            this.DrawTopPart(sb, sizeOfRomb);
+            this.DrawLineOfStars(sb, sizeOfRomb);
+            this.DrawBottomPart(sb, sizeOfRomb);
+
+            return sb.ToString();
+        }
+        private void DrawTopPart(StringBuilder sb, int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                sb.Append(new string(' ', n - i));
+                DrawLineOfStars(sb, i);
+            }
+        }
+        private void DrawBottomPart(StringBuilder sb, int n)
+        {
+            for (int i = n - 1; i >= 1; i--)
+            {
+                sb.Append(new string(' ', n - i));
+                DrawLineOfStars(sb, i);
+            }
+        }
+        private void DrawLineOfStars(StringBuilder sb, int numberOfStars)
+        {
+            for (int i = 1; i <= numberOfStars; i++)
+            {
+                sb.Append('*');
+                if (i < numberOfStars)
+                {
+                    sb.Append(' ');
+                }
+            }
+            sb.AppendLine();
+        }
+    }
+}
