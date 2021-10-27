f0fb538eaa09c446a244691423d5cf4648edac62
TSDilov <cv.dilov@abv.bg>
2021-10-27 10:56:22 +0300

    Added C# Fundamentals Objects and Classes code

---
+++ Programing Fundamentals - C#/Objects and Classes/Big Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger faktorial = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                faktorial *= i;
+            }
+            Console.WriteLine(faktorial);
+        }
+    }
+}
