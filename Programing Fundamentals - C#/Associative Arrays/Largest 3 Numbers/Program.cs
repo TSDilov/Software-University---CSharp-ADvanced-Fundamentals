358de05fc25aed26713f4a2de741ffa31c7a6023
TSDilov <cv.dilov@abv.bg>
2021-10-30 20:42:51 +0300

    Added C# Fundamentals Associative Arrays code

---
+++ Programing Fundamentals - C#/Associative Arrays/Largest 3 Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ",Console.ReadLine().Split().Select(int.Parse)
+                .OrderByDescending(n => n).Take(3).ToArray()));
+        }
+    }
+}
