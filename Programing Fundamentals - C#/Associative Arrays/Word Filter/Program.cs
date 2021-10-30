1eaeb5e52b1e453bf752727536344ee4d86c96e3
TSDilov <cv.dilov@abv.bg>
2021-10-30 20:44:40 +0300

    Added C# Fundamentals Associative Arrays code

---
+++ Programing Fundamentals - C#/Associative Arrays/Word Filter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            List<string> evenWords = new List<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length % 2 == 0)
+                {
+                    evenWords.Add(words[i]);
+                }
+            }
+            foreach (var word in evenWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
