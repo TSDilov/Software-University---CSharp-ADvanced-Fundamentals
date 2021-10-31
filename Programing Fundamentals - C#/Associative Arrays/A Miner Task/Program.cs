87597c2eadbe225761c4ec71521e400d13f9602f
TSDilov <cv.dilov@abv.bg>
2021-10-31 13:31:55 +0200

    Added C# Fundamentals Associative Arrays code

---
+++ Programing Fundamentals - C#/Associative Arrays/A Miner Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quantityOFResources = new Dictionary<string, int>();
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!quantityOFResources.ContainsKey(resource))
+                {
+                    quantityOFResources.Add(resource, quantity);
+                    continue;
+                }
+                quantityOFResources[resource] += quantity;               
+            }
+            foreach (var resource in quantityOFResources)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
