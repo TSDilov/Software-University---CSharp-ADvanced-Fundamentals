31e3ba7770667942c084730c2a07e35a00ba0712
TSDilov <cv.dilov@abv.bg>
2021-10-31 13:30:38 +0200

    Added C# Fundamentals Associative Arrays code

---
+++ Programing Fundamentals - C#/Associative Arrays/Count Chars in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountCharsinaString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ' && !counts.ContainsKey(text[i]))
+                {
+                    counts.Add(text[i], 1);
+                    continue;
+                }
+                if (text[i] != ' ')
+                {
+                    counts[text[i]]++;
+                }
+            }
+
+            foreach (var ch in counts)
+            {
+                Console.WriteLine($"{ch.Key} -> {ch.Value}");
+            }
+        }
+    }
+}
