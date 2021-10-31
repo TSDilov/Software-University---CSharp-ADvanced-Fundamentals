21b995e09ed5e884be2fa08b9cf3a25ac66f1a85
TSDilov <cv.dilov@abv.bg>
2021-10-31 13:36:35 +0200

    Added C# Fundamentals Associative Arrays code

---
+++ Programing Fundamentals - C#/Associative Arrays/SoftUni Parking/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> registeredUsers = new Dictionary<string, string>();
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                string format = command[0];
+                string user = command[1];
+
+                switch (format)
+                {
+                    case "register":
+                        string licenseNumber = command[2];
+                        if (!registeredUsers.ContainsKey(user))
+                        {
+                            registeredUsers.Add(user, licenseNumber);
+                            Console.WriteLine($"{user} registered {licenseNumber} successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ERROR: already registered with plate number {licenseNumber}");
+                        }
+                        break;
+                    case "unregister":
+                        if (registeredUsers.ContainsKey(user))
+                        {
+                            registeredUsers.Remove(user);
+                            Console.WriteLine($"{user} unregistered successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ERROR: user {user} not found");
+                        }
+                        break;
+                }
+            }
+            foreach (var us in registeredUsers)
+            {
+                Console.WriteLine($"{us.Key} => {us.Value}");
+            }
+        }
+    }
+}
