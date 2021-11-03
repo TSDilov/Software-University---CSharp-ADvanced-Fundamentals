53c7823c3c70393d801d96f6fe703d74b1ef96a3
TSDilov <cv.dilov@abv.bg>
2021-11-03 16:19:38 +0200

    Added C# Advance Iterators and Comparators code

---
+++ C# Advanced/Iterators and Comparators/Collection/ListyIterator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private  List<T> list;
+        private int currentIndex;
+
+        public ListyIterator(params T[] data)
+        {
+            this.list = new List<T>(data);
+            currentIndex = 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in list)
+            {
+                yield return item;
+            }
+        }
+
+        public bool HasNext()
+        {
+            return currentIndex < list.Count - 1;
+        }
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                currentIndex++;
+            }
+            return canMove;
+        }
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException(message:"Invalid Operation!");
+            }
+            Console.WriteLine(list[currentIndex]);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators/Collection/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            ListyIterator<string> myColection = new ListyIterator<string>();
+            while (command != "END")
+            {
+                string[] currentCommand = command.Split();
+                string exactCommand = currentCommand[0];
+                if (exactCommand == "Create")
+                {
+                    myColection = new ListyIterator<string>(currentCommand.Skip(1).ToArray());
+                }
+                else if (exactCommand == "Move")
+                {
+                    Console.WriteLine(myColection.Move());
+                }
+                else if (exactCommand == "HasNext")
+                {
+                    Console.WriteLine(myColection.HasNext());
+                }
+                else if (exactCommand == "Print")
+                {
+                    myColection.Print();
+                }
+                else if (exactCommand == "PrintAll")
+                {
+                    foreach (var item in myColection)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine();
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
