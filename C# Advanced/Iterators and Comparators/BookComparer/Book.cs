14f1959baf81de7682b525776b49e9c0b4778496
TSDilov <cv.dilov@abv.bg>
2021-11-03 16:16:03 +0200

    Added C# Advanced Iterators and Comparators code

---
+++ C# Advanced/Iterators and Comparators/BookComparer/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; private set; }
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year != other.Year)
+            {
+                return this.Year.CompareTo(other.Year);
+            }
+            return this.Title.CompareTo(other.Title);
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
