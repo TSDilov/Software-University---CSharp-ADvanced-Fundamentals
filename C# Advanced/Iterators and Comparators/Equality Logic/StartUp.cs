53f08c90f9c28cbc120be609ca8878342b312d5f
TSDilov <cv.dilov@abv.bg>
2021-11-03 16:25:56 +0200

    Added C# Advanced OOP Iterators and Comparators code

---
+++ C# Advanced/Iterators and Comparators/Equality Logic/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var sortedSet = new SortedSet<Person>();
+            var hashSet = new HashSet<Person>();
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var people = Console.ReadLine().Split();
+                var person = new Person(people[0], int.Parse(people[1]));
+                sortedSet.Add(person);
+                hashSet.Add(person);
+            }
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
