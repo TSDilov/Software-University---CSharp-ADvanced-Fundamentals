096ed5c739d6f4cb8c9a7dc9088465c0cad6db09
TSDilov <cv.dilov@abv.bg>
2021-11-03 16:21:12 +0200

    Added C# Advanced OOP Iterators and Comparators code

---
+++ C# Advanced/Iterators and Comparators/Stack/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackWithIterator
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> stack;
+        public Stack(params T[] data)
+        {
+            stack = new List<T>(data);
+        }
+        public void Push(params T[] elements)
+        {
+            foreach (var element in elements)
+            {
+                stack.Add(element);
+            }
+        }
+        public T Pop()
+        {
+            if (stack.Count == 0)
+            {
+                throw new ArgumentException(message:"No elements");
+            }
+            var lastIndex = stack.Count - 1;
+            var last = stack[lastIndex];
+            stack.RemoveAt(lastIndex);
+            return last;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = stack.Count - 1; i >= 0; i--)
+            {
+                yield return stack[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators/Stack/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace StackWithIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+            var command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (command[0] != "END")
+            {
+                if (command[0] == "Push")
+                {
+                    stack.Push(command.Skip(1).Select(e => e.Split(",").First()).ToArray());
+                }
+                else if (command[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (ArgumentException ae)
+                    {
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+                command = Console.ReadLine().Split();
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
