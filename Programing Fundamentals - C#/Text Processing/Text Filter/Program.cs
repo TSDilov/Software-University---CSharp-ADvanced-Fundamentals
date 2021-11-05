b7702e3caa6c8fbc5bf82f270e5fad673c16f2c0
TSDilov <cv.dilov@abv.bg>
2021-11-05 09:47:09 +0200

    Added C# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Text Processing/Text Filter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsForReplace = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var word in wordsForReplace)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
