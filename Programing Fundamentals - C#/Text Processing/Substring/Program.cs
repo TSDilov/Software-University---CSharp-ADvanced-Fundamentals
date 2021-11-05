b1dd98d3a765a645031bff01da423385f64cd79f
TSDilov <cv.dilov@abv.bg>
2021-11-05 09:45:55 +0200

    Added S# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Text Processing/Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordForRemove = Console.ReadLine();
+            string text = Console.ReadLine();
+            int indexOfWord = text.IndexOf(wordForRemove);
+
+            while (indexOfWord >= 0)
+            {
+                text = text.Remove(indexOfWord, wordForRemove.Length);
+                indexOfWord = text.IndexOf(wordForRemove);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
