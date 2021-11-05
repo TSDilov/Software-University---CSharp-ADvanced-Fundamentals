a5138af1421b4ad46c65320153ec734045f40bd9
TSDilov <cv.dilov@abv.bg>
2021-11-05 09:42:40 +0200

    Added C# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Text Processing/Reverse Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string word = Console.ReadLine();
+                if (word == "end")
+                {
+                    break;
+                }
+                string reverse = string.Empty;
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reverse += word[i];
+                }
+                Console.WriteLine($"{word} = {reverse}");
+            }
+            
+        }
+    }
+}
