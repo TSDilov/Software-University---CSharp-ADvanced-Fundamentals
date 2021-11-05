200c1e6c96eccf2f0c958a23c741007c6e49e924
TSDilov <cv.dilov@abv.bg>
2021-11-05 09:48:39 +0200

    Added C# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Text Processing/Digits, Letters and Other/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace DigitsLettersandOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder text = new StringBuilder();
+            text.Append(Console.ReadLine());
+            StringBuilder[] chars = { new StringBuilder(), new StringBuilder(), new StringBuilder() };
+
+            foreach (var @char in text.ToString())
+            {
+                if (char.IsDigit(@char))
+                {
+                    chars[0].Append(@char);
+                }
+                else if (char.IsLetter(@char))
+                {
+                    chars[1].Append(@char);
+                }
+                else
+                {
+                    chars[2].Append(@char);
+                }
+            }
+            foreach (var textWithChars in chars)
+            {
+                Console.WriteLine(textWithChars);
+            }
+        }
+    }
+}
