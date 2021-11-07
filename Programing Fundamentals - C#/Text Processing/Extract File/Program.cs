2154054dac7e6e7c2aa2ba2fef1c2622b73d63a6
TSDilov <cv.dilov@abv.bg>
2021-11-07 10:07:45 +0200

    Added C# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Text Processing/Extract File/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullPath = Console.ReadLine();
+            int lastIndexOfBreakLine = fullPath.LastIndexOf('\\');
+            string substringPath = fullPath.Substring(lastIndexOfBreakLine + 1);
+            int extensionIndex = substringPath.LastIndexOf('.');
+            string extension = substringPath.Substring(extensionIndex + 1);
+            string name = substringPath.Substring(0, substringPath.Length - extension.Length - 1);
+            Console.WriteLine($"File name: {name}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
