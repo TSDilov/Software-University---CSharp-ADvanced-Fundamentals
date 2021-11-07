84133a8d35851c073fc568eafa4447846bf9177d
TSDilov <cv.dilov@abv.bg>
2021-11-07 10:12:44 +0200

    Added C# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Text Processing/String Explosion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder input = new StringBuilder();
+            input.Append(Console.ReadLine());
+            int power = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (power > 0 && input[i] != '>' )
+                {
+                    input.Remove(i, 1);
+                    i--;
+                    power--;
+                }
+                else if (input[i] == '>')
+                {
+                    power += int.Parse(input[i + 1].ToString());
+
+                }
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
