de7b2835525c1b78f3e535217587a1de7f3892d3
TSDilov <cv.dilov@abv.bg>
2021-11-07 10:09:04 +0200

    Added C# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Text Processing/Caesar Cipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder crypted = new StringBuilder();
+            crypted.Append(Console.ReadLine());
+            StringBuilder unCrypted = new StringBuilder();
+
+            foreach (var @char in crypted.ToString())
+            {
+                unCrypted.Append((char)(@char + 3));
+            }
+            Console.WriteLine(unCrypted);
+        }
+    }
+}
