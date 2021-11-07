9400a941732905aa374d539112dbb4cb2c8fe437
TSDilov <cv.dilov@abv.bg>
2021-11-07 10:10:10 +0200

    Added C# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Text Processing/Multiply Big Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNumber = Console.ReadLine();
+            int secondNumber = int.Parse(Console.ReadLine());
+            int reminder = 0;
+
+            StringBuilder multiply = new StringBuilder();
+            if (firstNumber == "0" || secondNumber == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            for (int i = firstNumber.Length - 1; i >= 0; i--)
+            {
+                int currentDigit = int.Parse(firstNumber[i].ToString());
+                int product = currentDigit * secondNumber + reminder;
+                int result = product % 10;
+                reminder = product / 10;
+                multiply.Insert(0, result);
+            }
+            if (reminder > 0)
+            {
+                multiply.Insert(0, reminder);
+            }
+            Console.WriteLine(multiply);
+        }
+    }
+}
