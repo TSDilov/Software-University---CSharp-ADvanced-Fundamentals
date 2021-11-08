697db610e1cd068c9e863cda694118408203c4aa
TSDilov <cv.dilov@abv.bg>
2021-11-08 09:33:02 +0200

    Added C# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Regular Expressions/Match Dates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            MatchCollection dates = Regex.Matches(input,
+                @"\b(?<day>[0-9]{2})(?<separator>.)(?<month>[A-Z][a-z]{2})\k<separator>(?<year>[0-9]{4})\b");
+
+            foreach (Match date in dates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
