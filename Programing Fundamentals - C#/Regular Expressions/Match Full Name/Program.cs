1bed4dac6b7e562f01c2702bd136532bc6c302f0
TSDilov <cv.dilov@abv.bg>
2021-11-08 09:28:18 +0200

    Added C# Fundamentals Regular Expressions code

---
+++ Programing Fundamentals - C#/Regular Expressions/Match Full Name/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            MatchCollection names = Regex.Matches(input, @"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+
+            foreach (Match name in names)
+            {
+                Console.Write(name.Value + " ");
+            }
+        }
+    }
+}
