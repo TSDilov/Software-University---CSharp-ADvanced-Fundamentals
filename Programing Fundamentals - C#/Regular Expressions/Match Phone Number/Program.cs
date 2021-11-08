2391a5b72e91700570d0afa8aa2d45e0f997d720
TSDilov <cv.dilov@abv.bg>
2021-11-08 09:31:46 +0200

    Added C# Fundamentals Text Processing code

---
+++ Programing Fundamentals - C#/Regular Expressions/Match Phone Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            MatchCollection phones = Regex.Matches(input, @"\+[3][5][9](\-| )[2]\1[0-9]{3}\1[0-9]{4}\b");
+
+            Console.Write(string.Join(", ",phones));
+        }
+    }
+}
