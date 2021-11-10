f47e283017b9d7b69102be2d4fd49d52266a33f5
TSDilov <cv.dilov@abv.bg>
2021-11-10 15:47:35 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Simple Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> elements = new Stack<string>(Console.ReadLine().Split().Reverse());
+            while (elements.Count > 1)
+            {
+                int firstForCalculate = int.Parse(elements.Pop());
+                string calculator = elements.Pop();
+                int secondForCalculate = int.Parse(elements.Pop());
+                elements.Push(calculator == "+" ? (firstForCalculate + secondForCalculate).ToString() : 
+                    (firstForCalculate - secondForCalculate).ToString());
+            }
+            Console.WriteLine(elements.Pop());
+        }
+    }
+}
