1ccc48c0a2e3b7e170c5aad62b22633c3ba5e028
TSDilov <cv.dilov@abv.bg>
2021-11-10 15:52:27 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Supermarket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Queue<string> names = new Queue<string>();
+            while (name != "End")
+            {
+                if (name == "Paid")
+                {
+                    foreach (var addName in names)
+                    {
+                        Console.WriteLine(addName);
+                    }
+                    names.Clear();
+                    name = Console.ReadLine();
+                    continue;
+                }
+                names.Enqueue(name);
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
