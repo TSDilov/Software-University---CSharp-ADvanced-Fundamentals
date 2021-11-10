bd4e4adad9a89b1d77717bb0ea7dbd3f87c89553
TSDilov <cv.dilov@abv.bg>
2021-11-10 15:51:09 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Print Even Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> evenNumbers = new Queue<int>();
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Enqueue(number);
+                }
+            }
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
