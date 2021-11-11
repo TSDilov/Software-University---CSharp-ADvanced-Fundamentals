c5f38b23fa98d5b371d70395b2b33967464ba052
TSDilov <cv.dilov@abv.bg>
2021-11-11 09:41:02 +0200

    Added C# Fundamentals Exam Tasks code

---
+++ Programing Fundamentals - C#/Exam Tasks/The Lift/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheLift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            List<int> lifts = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int i = 0; i < lifts.Count; i++)
+            {
+                while (lifts[i] < 4)
+                {
+                    if (people <= 0)
+                    {
+                        break;
+                    }
+                    people--;
+                    lifts[i]++;
+                }
+            }
+            if (!lifts.All(w => w ==4) && people <= 0)
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", lifts));
+            }
+            else if (lifts.All(w => w == 4) && people > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {people} people in a queue!");
+                Console.WriteLine(string.Join(" ", lifts));
+            }
+            else if (lifts.All(w => w == 4) && people <= 0)
+            {
+                Console.WriteLine(string.Join(" ", lifts));
+            }
+        }
+    }
+}
