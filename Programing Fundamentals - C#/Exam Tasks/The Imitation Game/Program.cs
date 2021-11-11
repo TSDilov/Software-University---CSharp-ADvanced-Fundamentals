e085904325da2e85a04072ff7eddc563ebf35126
TSDilov <cv.dilov@abv.bg>
2021-11-11 09:48:02 +0200

    Added C# Fundamentals Exam Tasks code

---
+++ Programing Fundamentals - C#/Exam Tasks/The Imitation Game/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace TheImitationGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder message = new StringBuilder(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            while (command != "Decode")
+            {
+                string[] currentCommand = command.Split("|",StringSplitOptions.RemoveEmptyEntries);
+
+                switch (currentCommand[0])
+                {
+                    case "Move":
+                        string movedString = message.ToString().Substring(0, int.Parse(currentCommand[1]));
+                        message.Remove(0, int.Parse(currentCommand[1]));
+                        message.Append(movedString);
+                        break;
+                    case "Insert":
+                        int index = int.Parse(currentCommand[1]);
+                        string value = currentCommand[2];
+                        message.Insert(index, value);
+                        break;
+                    case "ChangeAll":
+                        while (message.ToString().Contains(currentCommand[1]))
+                        {
+                            message.Replace(currentCommand[1],currentCommand[2]);
+                        }
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"The decrypted message is: {message}");
+        }
+    }
+}
