c51b99d173dde42fd61694ab0e2aa6872fd49100
TSDilov <cv.dilov@abv.bg>
2021-11-12 09:59:27 +0200

    Added C# Fundamentals Exam Tasks code

---
+++ Programing Fundamentals - C#/Exam Tasks/Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> moreThanAverage = new List<int>();
+            double averageNumber = integers.Average();
+            moreThanAverage = integers.Where(i => i > averageNumber).OrderByDescending(i => i).ToList();
+            while (moreThanAverage.Count > 5)
+            {
+                moreThanAverage.RemoveAt(moreThanAverage.Count - 1);
+            }
+            if (moreThanAverage.Count == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ",moreThanAverage));
+            }
+            
+        }
+    }
+}
