09a69ae7c0f71fdc53cb240a790f685283fa1cae
TSDilov <cv.dilov@abv.bg>
2021-11-12 10:02:18 +0200

    Added C# Fundamentals Exam Tasks code

---
+++ Programing Fundamentals - C#/Exam Tasks/World Tour/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace WorldTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string command = Console.ReadLine();
+
+            while (command != "Travel")
+            {
+                string[] commands = command.Split(":");
+                switch (commands[0])
+                {
+                    case "Add Stop":
+                        int index = int.Parse(commands[1]);
+                        string text = commands[2];
+                        if (index >= 0 && index <= input.Length - 1)
+                        {
+                            input = input.Insert(index, text);
+                        }
+                            Console.WriteLine(input);                       
+                        break;
+                    case "Remove Stop":
+                        int startIndex = int.Parse(commands[1]);
+                        int endIndex = int.Parse(commands[2]);
+                        if (startIndex >= 0 && startIndex <= input.Length - 1 && endIndex >= 0 && endIndex <= input.Length - 1)
+                        {
+                            input = input.Remove(startIndex, endIndex - startIndex + 1);
+                        }
+                        Console.WriteLine(input);
+                        break;
+                    case "Switch":
+                        string oldText = commands[1];
+                        string newText = commands[2];
+                        if (input.Contains(oldText) && oldText != newText)
+                        {
+                            input = input.Replace(oldText, newText);
+                        }
+                        Console.WriteLine(input);
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {input}");
+        }
+    }
+}
