e9de4ef860908c0ac0e68c382b12b573b7bd4a7c
TSDilov <cv.dilov@abv.bg>
2021-11-12 09:47:10 +0200

    Added C# Fundamentals Exam Tasks code

---
+++ Programing Fundamentals - C#/Exam Tasks/SoftUni Reception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SoftUniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployee = int.Parse(Console.ReadLine());
+            int secondEmployee = int.Parse(Console.ReadLine());
+            int thirdEmployee = int.Parse(Console.ReadLine());
+            int customers = int.Parse(Console.ReadLine());
+
+            int hours = 0;
+
+            while (customers > 0)
+            {
+                customers -= firstEmployee + secondEmployee + thirdEmployee;
+                hours++;
+                if (hours % 4 == 0 && hours >= 4)
+                {
+                    hours++;
+                }
+            }
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
