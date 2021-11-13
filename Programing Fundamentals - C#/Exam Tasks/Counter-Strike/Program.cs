75fe6cecb82174b8e0587e01e2706d4db434b077
TSDilov <cv.dilov@abv.bg>
2021-11-13 12:41:13 +0200

    Added C# Fundamentals Exam Tasks code

---
+++ Programing Fundamentals - C#/Exam Tasks/Counter-Strike/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CounterStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+            string distance = Console.ReadLine();
+            int countOfWonBattles = 0;
+            bool dead = false;
+            while (distance != "End of battle")
+            {
+                if (initialEnergy < int.Parse(distance))
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {countOfWonBattles} won battles and {initialEnergy} energy");
+                    dead = true;
+                    break;
+                }
+                initialEnergy -= int.Parse(distance);
+                countOfWonBattles++;
+                if (countOfWonBattles % 3 == 0)
+                {
+                    initialEnergy += countOfWonBattles;
+                }
+              
+                distance = Console.ReadLine();
+            }
+            if (!dead)
+            {
+                Console.WriteLine($"Won battles: {countOfWonBattles}. Energy left: {initialEnergy}");
+            }
+      
+        }
+    }
+}
