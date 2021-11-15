55190a60170ab0ca8d88e7333fc08512fc86e7fe
TSDilov <cv.dilov@abv.bg>
2021-11-15 20:36:54 +0200

    Added C# Fundamentals Exam Tasks code

---
+++ Programing Fundamentals - C#/Exam Tasks/Fancy Barcodes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FancyBarcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\@#+[A-Z][A-Za-z0-9][A-Za-z0-9][A-Za-z0-9][A-Za-z0-9]+[A-Z]\@#+";
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                StringBuilder productGroup = new StringBuilder();
+                Match validBarcodes = Regex.Match(input, pattern);
+                if (!validBarcodes.Success)
+                {
+                    Console.WriteLine("Invalid barcode");
+                    continue;
+                }
+                MatchCollection groupNumber = Regex.Matches(input, "[0-9]");
+                foreach (Match item in groupNumber)
+                {
+                    productGroup.Append(item);
+                }
+                if (productGroup.Length == 0)
+                {
+                    Console.WriteLine("Product group: 00");
+                }
+                else
+                {
+                    Console.WriteLine($"Product group: {productGroup}");
+                }
+            }
+        }
+    }
+}
