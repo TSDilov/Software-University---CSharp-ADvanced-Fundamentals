ab422e6301ef2718eb3ed884acdfb8c16f711a18
TSDilov <cv.dilov@abv.bg>
2021-11-16 10:30:56 +0200

    Added C# Fundamentals Exam Tasks code

---
+++ Programing Fundamentals - C#/Exam Tasks/MuOnline/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MuOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> rooms = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries).ToList();
+            int health = 100;
+            int bitcoins = 0;
+            bool dead = false;
+            foreach (var room in rooms)
+            {
+                string[] action = room.Split();
+
+                if (action[0] != "potion" && action[0] != "chest")
+                {
+                    health -= int.Parse(action[1]);
+                    string monster = action[0];
+                    if (health > 0)
+                    {
+                        Console.WriteLine($"You slayed {monster}.");
+                    }
+                    else
+                    {
+                        dead = true;
+                        Console.WriteLine($"You died! Killed by {monster}.");
+                        Console.WriteLine($"Best room: {rooms.IndexOf(room) + 1}");
+                        return;
+                    }
+                }
+                else if (action[0] == "potion")
+                {
+                    int healthBefore = health;
+                    health += int.Parse(action[1]);
+                    int healAmount = int.Parse(action[1]);
+                    if (health > 100)
+                    {
+                        health = 100;
+                        healAmount = health - healthBefore;
+                    }
+                    Console.WriteLine($"You healed for {healAmount} hp.");
+                    Console.WriteLine($"Current health: {health} hp.");
+                }
+                else if (action[0] == "chest")
+                {
+                    bitcoins += int.Parse(action[1]);
+                    Console.WriteLine($"You found {action[1]} bitcoins.");
+                }
+            }
+            if (!dead)
+            {
+                Console.WriteLine("You've made it!");
+                Console.WriteLine($"Bitcoins: {bitcoins}");
+                Console.WriteLine($"Health: {health}");
+            }
+        }
+    }
+}
