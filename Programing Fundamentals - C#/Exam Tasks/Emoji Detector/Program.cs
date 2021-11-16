61744ab1fe0cc9b1be3f558ae87b755d55e92b3c
TSDilov <cv.dilov@abv.bg>
2021-11-16 10:48:40 +0200

    Added C# Fundamentals Exam Tasks code

---
+++ Programing Fundamentals - C#/Exam Tasks/Emoji Detector/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EmojiDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([:*])\1([A-Z][a-z]{2,})\1{2}";
+            MatchCollection matchOfDigits = Regex.Matches(input, "[0-9]");
+            int coolThreshold = 1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    coolThreshold *= input[i] - 48;
+                }
+            }
+            Console.WriteLine($"Cool threshold: {coolThreshold}");
+            MatchCollection emojis = Regex.Matches(input, pattern);
+            Console.WriteLine($"{emojis.Count} emojis found in the text. The cool ones are:");
+            foreach (Match emoji in emojis)
+            {
+                int coolness = 0;
+                string currentEmoji = emoji.Groups[2].Value;
+                for (int i = 0; i < currentEmoji.Length; i++)
+                {
+                    coolness += currentEmoji[i];
+                }
+                if (coolness >= coolThreshold)
+                {
+                    Console.WriteLine(emoji);
+                }
+            }
+        }
+    }
+}
