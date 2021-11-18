9d2d3720a9f140847a7cf91caff1a46a86e713e3
TSDilov <cv.dilov@abv.bg>
2021-11-18 10:28:08 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Hot Potato/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPatato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            int mover = int.Parse(Console.ReadLine());
+            Queue<string> childrens = new Queue<string>();
+            foreach (var name in names)
+            {
+                childrens.Enqueue(name);
+            }
+
+            while (childrens.Count > 1)
+            {
+                for (int i = 0; i < mover - 1; i++)
+                {
+                    string ChildrenWithPatato = childrens.Dequeue();
+                    childrens.Enqueue(ChildrenWithPatato);
+                }
+                string removed = childrens.Dequeue();
+                Console.WriteLine("Removed " + removed);
+            }
+            Console.WriteLine($"Last is {childrens.Dequeue()}");
+        }
+    }
+}
