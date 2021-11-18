58067e426e9c791a436d56dcab812f336e610936
TSDilov <cv.dilov@abv.bg>
2021-11-18 10:30:06 +0200

    Added C# Fundamentals Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Traffic Jam/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOFCarsThatMoveInGreen = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+            int passed = 0;
+            while (true)
+            {
+                string pointer = Console.ReadLine();
+                if (pointer == "end")
+                {
+                    break;
+                }
+                else if (pointer == "green")
+                {
+                    for (int i = 0; i < numberOFCarsThatMoveInGreen; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            string car = cars.Dequeue();
+                            Console.WriteLine(car + " passed!");
+                            passed++;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(pointer);
+                }
+            }
+            Console.WriteLine($"{passed} cars passed the crossroads.");
+        }
+    }
+}
