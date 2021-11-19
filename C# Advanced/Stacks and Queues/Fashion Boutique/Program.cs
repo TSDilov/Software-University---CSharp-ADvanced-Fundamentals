097566a4b927b3a74b4f24eed29b9c61eac97955
TSDilov <cv.dilov@abv.bg>
2021-11-19 10:21:07 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Fashion Boutique/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int capacityOfRack = int.Parse(Console.ReadLine());
+            int currentClothesOnRack = 0;
+            int rackCounter = 1;
+            int count = clothes.Count;
+            for (int i = 0; i < count; i++)
+            {
+                if (currentClothesOnRack + clothes.Peek() > capacityOfRack)
+                {
+                    currentClothesOnRack = 0;
+                    rackCounter++;
+                }
+                currentClothesOnRack += clothes.Pop();
+            }
+            Console.WriteLine(rackCounter);
+        }
+    }
+}
