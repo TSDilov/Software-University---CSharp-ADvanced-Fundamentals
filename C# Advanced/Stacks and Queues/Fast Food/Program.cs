e96d35dfd6d804dfffa990ff1ffbbfcc7d5a2b9c
TSDilov <cv.dilov@abv.bg>
2021-11-19 10:20:01 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Fast Food/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityForDay = int.Parse(Console.ReadLine());
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            int biggestOrder = orders.Max();
+            int count = orders.Count;
+            for (int i = 0; i < count; i++)
+            {               
+                if (quantityForDay >= orders.Peek())
+                {
+                    quantityForDay -= orders.Dequeue();
+                    continue;
+                }
+                break;
+            }
+            Console.WriteLine(biggestOrder);
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ",orders)}");
+            }
+        }
+    }
+}
