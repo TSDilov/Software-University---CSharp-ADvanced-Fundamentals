a1e0cdb0adc395e0011f28fa891b19122514a232
TSDilov <cv.dilov@abv.bg>
2021-11-19 10:22:19 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Songs Queue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstSongs = Console.ReadLine().Split(", ");
+            Queue<string> songs = new Queue<string>();
+            for (int i = 0; i < firstSongs.Length; i++)
+            {
+                songs.Enqueue(firstSongs[i]);
+            }
+            string command = Console.ReadLine();
+            while (songs.Count > 0)
+            {
+                int indexSeparator = command.IndexOf(' ');
+                switch (indexSeparator == -1 ? command : command.Substring(0,indexSeparator))
+                {
+                    case "Play":
+                        songs.Dequeue();
+                        break;
+                    case "Add":
+                        string songToAdd = command.Substring(indexSeparator + 1);
+                        if (!songs.Contains(songToAdd))
+                        {
+                            songs.Enqueue(songToAdd);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{songToAdd} is already contained!");
+                        }
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songs));
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
