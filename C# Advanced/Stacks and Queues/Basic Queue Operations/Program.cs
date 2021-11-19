f5e72599532614304b4b63bee5b12beccd12ab5a
TSDilov <cv.dilov@abv.bg>
2021-11-19 10:17:39 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Basic Queue Operations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersForStackCommands = Console.ReadLine().Split()
+                .Select(int.Parse).ToArray();
+            
+            Queue<int> numbers = new Queue<int>(Console.ReadLine().Split()
+                .Select(int.Parse));
+            int numbersForDequeue = numbersForStackCommands[1];
+            int numbersForSearch = numbersForStackCommands[2];
+
+            for (int i = 0; i < numbersForDequeue; i++)
+            {
+                numbers.Dequeue();
+            }
+            if (numbers.Contains(numbersForSearch))
+            {
+                Console.WriteLine("true");
+            }
+            else if (numbers.Count <= 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine($"{numbers.Min()}");
+            }
+        }
+    }
+}
