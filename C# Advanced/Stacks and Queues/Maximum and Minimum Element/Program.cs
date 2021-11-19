8e37f58b921ec09473902dbc6dd815a26b81abac
TSDilov <cv.dilov@abv.bg>
2021-11-19 10:18:55 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Maximum and Minimum Element/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumandMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var numbers = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] operation = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (operation[0] == 1)
+                {
+                    numbers.Push(operation[1]);
+                }
+                else if (operation[0] == 2 && numbers.Count > 0)
+                {
+                    numbers.Pop();
+                }
+                else if (operation[0] == 3 && numbers.Count > 0)
+                {
+                    Console.WriteLine(numbers.Max());
+                }
+                else if (operation[0] == 4 && numbers.Count > 0)
+                {
+                    Console.WriteLine(numbers.Min());
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
