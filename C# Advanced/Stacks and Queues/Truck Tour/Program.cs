a35a7b474c6f01d26398cfc5e657cad30d8ec09c
TSDilov <cv.dilov@abv.bg>
2021-11-20 16:51:44 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Truck Tour/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<int[]> petrolPumps = new Queue<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] currentPump = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int fuel = currentPump[0];
+                int distance = currentPump[1];
+                petrolPumps.Enqueue(new int[] { fuel, distance });
+            }
+            
+            int startIndex = 0;
+            
+            while (petrolPumps .Count == n)
+            {
+                int currentFuel = 0;
+                foreach (var pump in petrolPumps)
+                {
+                    int petrolFromPump = pump[0];
+                    int distanceToNextpump = pump[1];
+                    currentFuel += petrolFromPump;
+                    currentFuel -= distanceToNextpump;
+                    if (currentFuel < 0)
+                    {
+                        int[] element = petrolPumps.Dequeue();
+                        petrolPumps.Enqueue(element);
+                        startIndex++;
+                        break;
+                    }
+                }
+                if (currentFuel >= 0)
+                {
+                    petrolPumps.Dequeue();
+                    Console.WriteLine(startIndex);
+                }
+            }
+        }
+    }
+}
