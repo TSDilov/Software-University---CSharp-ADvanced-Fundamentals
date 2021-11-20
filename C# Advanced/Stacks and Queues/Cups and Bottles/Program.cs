7ca6f54688e4ad01d35492f975e69be2c9916236
TSDilov <cv.dilov@abv.bg>
2021-11-20 16:57:51 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Cups and Bottles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CupsandBottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> cups = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).Reverse());
+            Stack<int> bottles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int wastedWater = 0;
+
+            while (cups.Count > 0 && bottles.Count > 0)
+            {
+                if (cups.Peek() - bottles.Peek() <= 0)
+                {
+                    wastedWater += Math.Abs(cups.Peek() - bottles.Peek());
+                    cups.Pop();
+                }
+                else
+                {
+                    cups.Push(cups.Pop() - bottles.Peek());
+                }
+                bottles.Pop();
+            }
+            if (cups.Count == 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+            }
+            else
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+            }
+        }
+    }
+}
