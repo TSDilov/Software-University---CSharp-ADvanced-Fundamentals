86ca9dc2984c2ec8def88c42f3b4ffb532f81571
TSDilov <cv.dilov@abv.bg>
2021-11-20 16:55:32 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Crossroads/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLightTime = int.Parse(Console.ReadLine());
+            int windowTime = int.Parse(Console.ReadLine());
+            int count = 0;
+            Queue<string> carsOnCrossroad = new Queue<string>();
+            string carOrCommand = Console.ReadLine();
+            while (carOrCommand != "END")
+            {
+                int greenLightTimeRemaining = greenLightTime;
+                int windowTimeLEft = windowTime;
+                
+                switch (carOrCommand)
+                {
+                    case "green":
+                        while (greenLightTimeRemaining > 0 && carsOnCrossroad.Any())
+                        {
+                            string carThatPasses = carsOnCrossroad.Dequeue();
+                            greenLightTimeRemaining -= carThatPasses.Length;
+                            if (greenLightTimeRemaining >= 0)
+                            {
+                                count++;
+                            }
+                            else
+                            {
+                                windowTimeLEft += greenLightTimeRemaining;
+                                if (windowTimeLEft < 0)
+                                {
+                                    Console.WriteLine("A crash happened!");
+                                    Console.WriteLine($"{carThatPasses} was hit at {carThatPasses[carThatPasses.Length + windowTimeLEft]}.");
+                                    return;
+                                }
+                                count++;
+                            }
+                        }
+                        break;
+                    default:
+                        carsOnCrossroad.Enqueue(carOrCommand);
+                        break;
+                }
+                carOrCommand = Console.ReadLine();
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{count} total cars passed the crossroads.");
+        }
+    }
+}
