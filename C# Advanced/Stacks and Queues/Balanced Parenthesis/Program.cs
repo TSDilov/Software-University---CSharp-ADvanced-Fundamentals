8550c627f14bd2c8f7bdf3a5400f229ee548fc50
TSDilov <cv.dilov@abv.bg>
2021-11-20 16:52:56 +0200

    Added C# Advanced Stacks and Queues code

---
+++ C# Advanced/Stacks and Queues/Balanced Parenthesis/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parenthesis = Console.ReadLine();
+            Stack<char> balancedOrNot = new Stack<char>();
+            bool isBalanced = true;
+
+            foreach (var symbol in parenthesis)
+            {
+
+                if (symbol == '{' || symbol == '[' || symbol == '(')
+                {
+                    balancedOrNot.Push(symbol);
+                }
+                else
+                {
+                    if (balancedOrNot.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                    if (symbol == '}' && balancedOrNot.Peek() == '{')
+                    {
+                        balancedOrNot.Pop();
+                    }
+                    else if (symbol == ']' && balancedOrNot.Peek() == '[')
+                    {
+                        balancedOrNot.Pop();
+                    }
+                    else if (symbol == ')' && balancedOrNot.Peek() == '(')
+                    {
+                        balancedOrNot.Pop();
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                    }
+                }
+            }
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
