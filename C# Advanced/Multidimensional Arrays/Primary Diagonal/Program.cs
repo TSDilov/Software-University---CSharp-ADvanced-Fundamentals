9c4f138b70b1b3778d5779a571a7eb82827039f1
TSDilov <cv.dilov@abv.bg>
2021-11-21 11:45:21 +0200

    Added C# Advanced Multidimensional Arrays code

---
+++ C# Advanced/Multidimensional Arrays/Primary Diagonal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col< matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }               
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                sum += matrix[row, row];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
