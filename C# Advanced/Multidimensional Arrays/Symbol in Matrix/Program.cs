b889a6326e496b975551183c45e540603f676794
TSDilov <cv.dilov@abv.bg>
2021-11-21 11:46:58 +0200

    Added C# Advanced Multidimensional Arrays code

---
+++ C# Advanced/Multidimensional Arrays/Symbol in Matrix/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SymbolinMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+            for (int row = 0; row < size; row++)
+            {
+                string characters = Console.ReadLine();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = characters[col];
+                }
+            }
+            char lookingSymbol = char.Parse(Console.ReadLine());
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (matrix[row,col] == lookingSymbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return; 
+                    }
+                }
+            }
+            Console.WriteLine($"{lookingSymbol} does not occur in the matrix");
+        }
+    }
+}
