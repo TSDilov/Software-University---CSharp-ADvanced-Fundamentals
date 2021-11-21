8af90039c9bcc70cce3a6d90a45760453eed0ca6
TSDilov <cv.dilov@abv.bg>
2021-11-21 11:43:31 +0200

    Added C# Advanced Multidimensional Arrays code

---
+++ C# Advanced/Multidimensional Arrays/Sum Matrix Columns/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[size[0], size[1]];
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];                    
+                }
+            }
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+        }
+    }
+}
