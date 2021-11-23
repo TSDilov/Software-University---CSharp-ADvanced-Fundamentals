bee833f385a6cc006e4b446a029a0e4bbfc83f8c
TSDilov <cv.dilov@abv.bg>
2021-11-23 10:43:14 +0200

    Added C# Advanced Multidimensional Arrays code

---
+++ C# Advanced/Multidimensional Arrays/Snake Moves/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string snake = Console.ReadLine();
+            char[,] matrix = new char[size[0], size[1]];
+            int counter = 0;
+            bool isLeftToRight = true;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (isLeftToRight)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = snake[counter++];
+                        if (counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                    }
+                    isLeftToRight = false;
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[counter++];
+                        if (counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                    }
+                    isLeftToRight = true;
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
