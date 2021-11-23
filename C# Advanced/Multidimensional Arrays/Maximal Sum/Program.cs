2f09b0a625d3538c2f39c6c8b90a700330996435
TSDilov <cv.dilov@abv.bg>
2021-11-23 10:40:41 +0200

    Added C# Advanced Multidimensional Arrays code

---
+++ C# Advanced/Multidimensional Arrays/Maximal Sum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[size[0], size[1]];
+            for (int row = 0; row < size[0]; row++)
+            {
+                int[] numbers = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < size[1]; col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+            long maxSum = long.MinValue;
+            int maxSumRow = 0;
+            int maxSumCol = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+                        matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                        matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxSumRow = row;
+                        maxSumCol = col;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int row = maxSumRow; row < maxSumRow + 3; row++)
+            {
+                for (int col = maxSumCol; col < maxSumCol + 3; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
