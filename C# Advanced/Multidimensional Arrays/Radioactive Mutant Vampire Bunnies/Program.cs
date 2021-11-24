a5f1d8e69c3a8274cbf9cf866802603712743d7a
TSDilov <cv.dilov@abv.bg>
2021-11-24 10:00:04 +0200

    Added C# Advanced Multidimensional Arrays code

---
+++ C# Advanced/Multidimensional Arrays/Radioactive Mutant Vampire Bunnies/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RadioactiveMutantVampireBunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            char[,] matrix = new char[size[0], size[1]];
+            int playerRow = 0;
+            int playerCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string input = Console.ReadLine();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                    if (matrix[row, col] == 'P')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+            string directions = Console.ReadLine();
+
+            foreach (var direction in directions)
+            {
+                int nextRow = 0;
+                int nextCol = 0;
+                switch (direction)
+                {
+                    case 'L':
+                        nextCol = -1;
+                        break;
+                    case 'R':
+                        nextCol = 1;
+                        break;
+                    case 'U':
+                        nextRow = -1;
+                        break;
+                    case 'D':
+                        nextRow = 1;
+                        break;
+
+                }
+                bool hasWon = false;
+                bool isDead = false;
+                
+                matrix[playerRow, playerCol] = '.';
+                if (!IsInside(matrix, playerRow + nextRow, playerCol + nextCol))
+                {
+                    hasWon = true;
+                }
+                else
+                {
+                    playerRow += nextRow;
+                    playerCol += nextCol;
+                    
+                    if (matrix[playerRow, playerCol] == 'B')
+                    {
+                        isDead = true;
+                    }
+                    else
+                    {
+                        matrix[playerRow, playerCol] = 'P';
+                    }
+                }
+
+                List<int[]> bunniesCordinates = new List<int[]>();
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (matrix[row, col] == 'B')
+                        {
+                            bunniesCordinates.Add(new int[] { row, col });
+                        }
+                    }
+                }
+                foreach (var bunny in bunniesCordinates)
+                {
+                    int bunnyRow = bunny[0];
+                    int bunnyCol = bunny[1];
+                    if (IsInside(matrix, bunnyRow + 1, bunnyCol))
+                    {
+                        if (matrix[bunnyRow + 1, bunnyCol] == 'P')
+                        {
+                            isDead = true;
+                        }
+                        matrix[bunnyRow + 1, bunnyCol] = 'B';
+                    }
+                    if (IsInside(matrix, bunnyRow - 1, bunnyCol))
+                    {
+                        if (matrix[bunnyRow - 1, bunnyCol] == 'P')
+                        {
+                            isDead = true;
+                        }
+                        matrix[bunnyRow - 1, bunnyCol] = 'B';
+                    }
+                    if (IsInside(matrix, bunnyRow, bunnyCol + 1))
+                    {
+                        if (matrix[bunnyRow, bunnyCol + 1] == 'P')
+                        {
+                            isDead = true;
+                        }
+                        matrix[bunnyRow, bunnyCol + 1] = 'B';
+                    }
+                    if (IsInside(matrix, bunnyRow, bunnyCol - 1))
+                    {
+                        if (matrix[bunnyRow, bunnyCol - 1] == 'P')
+                        {
+                            isDead = true;
+                        }
+                        matrix[bunnyRow, bunnyCol - 1] = 'B';
+                    }
+                }
+
+                if (hasWon)
+                {
+                    Print(matrix);
+                    Console.WriteLine($"won: {playerRow} {playerCol}");
+                    Environment.Exit(0);
+                }
+                if (isDead)
+                {
+                    Print(matrix);
+                    Console.WriteLine($"dead: {playerRow} {playerCol}");
+                    Environment.Exit(0); 
+                }
+            }
+
+        }
+        private static void Print(char[,] matrix) 
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        private static bool IsInside(char[,] board, int row, int col)
+        {
+            return row >= 0 && row < board.GetLength(0) &&
+                col >= 0 && col < board.GetLength(1);
+        }
+    }
+}
