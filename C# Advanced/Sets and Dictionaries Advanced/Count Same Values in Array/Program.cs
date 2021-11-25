31a96c2f492c8ba69e3062128496c733a36fe064
TSDilov <cv.dilov@abv.bg>
2021-11-25 11:30:05 +0200

    Added C# Advanced Sets and Dictionaries code

---
+++ C# Advanced/Sets and Dictionaries Advanced/Count Same Values in Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValuesinArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Dictionary<double, int> repeatingNumbers = new Dictionary<double, int>();
+            foreach (var number in numbers)
+            {
+                if (!repeatingNumbers.ContainsKey(number))
+                {
+                    repeatingNumbers.Add(number, 0);
+                }
+                repeatingNumbers[number]++;
+            }
+            foreach (var number in repeatingNumbers)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
