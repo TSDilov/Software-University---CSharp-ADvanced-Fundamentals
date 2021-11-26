c7ece98bb1926dd91bc23d5bf2608bcb1acff68b
TSDilov <cv.dilov@abv.bg>
2021-11-26 10:54:02 +0200

    Added C# Advanced Sets and Dictionaries code

---
+++ C# Advanced/Sets and Dictionaries Advanced/SoftUni Party/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string guessCode = Console.ReadLine();
+            HashSet<string> vip = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+            while (guessCode != "PARTY")
+            {
+                if (char.IsDigit(guessCode[0]))
+                {
+                    vip.Add(guessCode);
+                }
+                else
+                {
+                    regular.Add(guessCode);
+                }
+                guessCode = Console.ReadLine();
+            }
+            string guessWhoActuallyArreve = Console.ReadLine();
+            while (guessWhoActuallyArreve != "END")
+            {
+                if (char.IsDigit(guessWhoActuallyArreve[0]))
+                {
+                    vip.Remove(guessWhoActuallyArreve);
+                }
+                else
+                {
+                    regular.Remove(guessWhoActuallyArreve);
+                }
+                guessWhoActuallyArreve = Console.ReadLine();
+            }
+            Console.WriteLine(vip.Count + regular.Count);
+            foreach (var v in vip)
+            {
+                Console.WriteLine(v);
+            }
+            foreach (var r in regular)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
