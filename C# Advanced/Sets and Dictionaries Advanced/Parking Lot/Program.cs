989c341b452b76411a0ec32a46ecb37f05b2067a
TSDilov <cv.dilov@abv.bg>
2021-11-26 10:52:59 +0200

    Added C# Advanced Sets and Dictionaries code

---
+++ C# Advanced/Sets and Dictionaries Advanced/Parking Lot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split(", ");
+            HashSet<string> carNumbers = new HashSet<string>();
+            while (command[0] != "END")
+            {
+                string inOrOut = command[0];
+                string carNumber = command[1];
+                if (inOrOut == "IN")
+                {
+                    carNumbers.Add(carNumber);
+                }
+                else
+                {
+                    carNumbers.Remove(carNumber);
+                }
+                command = Console.ReadLine().Split(", ");
+            }
+            if (carNumbers.Count > 0)
+            {
+                foreach (var number in carNumbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
