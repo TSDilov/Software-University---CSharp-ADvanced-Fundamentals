33dafc913084357091eda9512e2f45ec634e53d0
TSDilov <cv.dilov@abv.bg>
2021-11-27 11:23:12 +0200

    Added C# Advanced Sets and Dictionaries code

---
+++ C# Advanced/Sets and Dictionaries Advanced/Periodic Table/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfListsOfElements = int.Parse(Console.ReadLine());
+            SortedSet<string> compounds = new SortedSet<string>();
+            for (int i = 0; i < numberOfListsOfElements; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    compounds.Add(elements[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", compounds));
+        }
+    }
+}
