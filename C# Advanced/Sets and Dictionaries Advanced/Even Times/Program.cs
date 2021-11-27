2ce98c6cc749a8b3159b37c16f27656525de0a14
TSDilov <cv.dilov@abv.bg>
2021-11-27 11:25:05 +0200

    Added C# Advanced Sets and Dictionaries

---
+++ C# Advanced/Sets and Dictionaries Advanced/Even Times/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfReadLines = int.Parse(Console.ReadLine());
+            Dictionary<int, int> ocurrancesOfNumber = new Dictionary<int, int>();
+            for (int i = 0; i < numberOfReadLines; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!ocurrancesOfNumber.ContainsKey(number))
+                {
+                    ocurrancesOfNumber.Add(number, 0);
+                }
+                ocurrancesOfNumber[number]++;
+            }
+            Console.WriteLine(ocurrancesOfNumber.First(n => n.Value % 2 == 0).Key);
+        }
+    }
+}
