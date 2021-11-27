31c1e5f34f20ef5e91b201f78aeed39fdfe18200
TSDilov <cv.dilov@abv.bg>
2021-11-27 11:11:27 +0200

    Added C# Advanced Sets and Dictionaries code

---
+++ C# Advanced/Sets and Dictionaries Advanced/Unique Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfReadLines = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+            for (int i = 0; i < numberOfReadLines; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
