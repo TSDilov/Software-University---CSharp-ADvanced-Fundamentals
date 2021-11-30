9b790f8257a5aab0a1450b09e7e3b8e1de8c8592
TSDilov <cv.dilov@abv.bg>
2021-11-30 10:08:54 +0200

    Added C# Advanced Sets and Dictionaries code

---
+++ C# Advanced/Sets and Dictionaries Advanced/Count Symbols/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            SortedDictionary<char, int> characters = new SortedDictionary<char, int>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!characters.ContainsKey(text[i]))
+                {
+                    characters.Add(text[i],0);
+                }
+                characters[text[i]]++;
+            }
+            foreach (var @char in characters)
+            {
+                Console.WriteLine($"{@char.Key}: {@char.Value} time/s");
+            }
+        }
+    }
+}
