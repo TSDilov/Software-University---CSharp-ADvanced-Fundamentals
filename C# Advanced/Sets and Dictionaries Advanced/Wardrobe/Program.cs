ef4684b83cc1932658af656f00ef6ad4821e2e93
TSDilov <cv.dilov@abv.bg>
2021-11-30 10:14:56 +0200

    Added C# Advanced Sets and Dictionaries code

---
+++ C# Advanced/Sets and Dictionaries Advanced/Wardrobe/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfClothes = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, int>> colorsOfClothes = new Dictionary<string, Dictionary<string, int>>(); 
+            for (int i = 0; i < numberOfClothes; i++)
+            {
+                string[] puttingClothes = Console.ReadLine().Split(" -> ");
+                string color = puttingClothes[0];
+                string[] typeOfClothe = puttingClothes[1].Split(',');
+                if (!colorsOfClothes.ContainsKey(color))
+                {
+                    colorsOfClothes.Add(color, new Dictionary<string, int>());
+                }
+                foreach (var clothe in typeOfClothe)
+                {
+                    if (!colorsOfClothes[color].ContainsKey(clothe))
+                    {
+                        colorsOfClothes[color].Add(clothe, 0);
+                    }
+                    colorsOfClothes[color][clothe]++;
+                }
+            }   
+            string[] lookingClothe = Console.ReadLine().Split();
+            foreach (var color in colorsOfClothes)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+                foreach (var (clothe, count) in color.Value)
+                {
+                    if (clothe == lookingClothe[1] && color.Key == lookingClothe[0])
+                    {
+                        Console.WriteLine($"* {clothe} - {count} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothe} - {count}");
+                    }
+                }
+            }
+        }
+    }
+}
