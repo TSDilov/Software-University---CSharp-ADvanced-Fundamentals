f0e6a1a1a6a29453f257a2dca1af722874d36113
TSDilov <cv.dilov@abv.bg>
2021-12-01 10:09:23 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/Sort Even Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            List<int> evenNumbers = new List<int>();
+            evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+            Console.WriteLine(string.Join(", ", evenNumbers.OrderBy(n => n)));
+        }
+    }
+}
