165c4571c257d3c01dfb3ff626d261119c83dbb9
TSDilov <cv.dilov@abv.bg>
2021-12-01 10:16:12 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/Add VAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prices = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse).ToList();
+            prices = prices.Select(p => p + p * 0.2).ToList();
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:F2}");
+            }
+        }
+    }
+}
