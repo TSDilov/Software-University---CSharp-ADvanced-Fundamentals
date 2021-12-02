5806eb8608581a932f9c6d19336e3de56e9c11ac
TSDilov <cv.dilov@abv.bg>
2021-12-02 10:23:58 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/Reverse And Exclude/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int[]> reverse = i =>
+            {
+                for (int j = 0; j < i.Length / 2; j++)
+                {
+                    int temp = i[j];
+                    i[j] = i[i.Length - 1 - j];
+                    i[i.Length - 1 - j] = temp;
+                }
+            };
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int numberToDevide = int.Parse(Console.ReadLine());
+            Predicate<int> filter = num => num % numberToDevide != 0;
+            reverse(numbers);
+            Console.WriteLine(string.Join(" ", numbers.ToList().FindAll(filter)));
+        }
+    }
+}
