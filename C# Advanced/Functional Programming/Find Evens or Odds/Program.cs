2593f713849a0db500b5b5686a7454bdad9d2c38
TSDilov <cv.dilov@abv.bg>
2021-12-02 10:14:36 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/Find Evens or Odds/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindEvensorOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fromTo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var numbers = new List<int>();
+            for (int i = fromTo[0]; i <= fromTo[1]; i++)
+            {
+                numbers.Add(i);
+            }
+            Predicate<int> evenOrOdd = null;
+            var filter = Console.ReadLine();
+            switch (filter)
+            {
+                case "odd":
+                    evenOrOdd = n => n % 2 != 0;
+                    break;
+                case "even":
+                    evenOrOdd = n => n % 2 == 0;
+                    break;
+            }
+            Console.WriteLine(string.Join(" ", numbers.FindAll(evenOrOdd)));
+
+        }
+    }
+}
