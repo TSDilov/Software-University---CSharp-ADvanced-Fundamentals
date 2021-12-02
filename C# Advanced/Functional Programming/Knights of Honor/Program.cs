a9f3634a11c94f07ad853a4d14a0aaaabb3563b8
TSDilov <cv.dilov@abv.bg>
2021-12-02 10:11:14 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/Knights of Honor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsofHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = str => Console.WriteLine("Sir " + str);
+            Console.ReadLine().Split().ToList().ForEach(print);
+        }
+    }
+}
