5d7b3d69b208be19dcd6b55434e5af8529cd2d1e
TSDilov <cv.dilov@abv.bg>
2021-12-02 10:13:00 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/Custom Min Function/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int[], int> smallest = i =>
+            {
+                int smallest = int.MaxValue;
+                for (int j = 0; j < i.Length; j++)
+                {
+                    if (i[j] < smallest)
+                    {
+                        smallest = i[j];
+                    }
+                }
+                return smallest;
+            };
+            Console.WriteLine(smallest(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()));
+        }
+        
+    }
+}
