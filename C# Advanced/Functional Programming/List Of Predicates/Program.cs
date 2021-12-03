43aa50f5c32ffb3209ebf8eb7d4daa97c54cd8c2
TSDilov <cv.dilov@abv.bg>
2021-12-03 09:24:57 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/List Of Predicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListofPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int[] deviders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = i + 1;
+            }
+            int[] devisible = Array.FindAll(array, n =>
+            {
+                bool isDividable = false;
+                for (int i = 0; i < deviders.Length; i++)
+                {
+                    if (n % deviders[i] == 0)
+                    {
+                        isDividable = true;
+                    }
+                    if (isDividable && n % deviders[i] != 0)
+                    {
+                        isDividable = false;
+                        break;
+                    }
+                }
+                return isDividable;
+            });
+            Console.WriteLine(string.Join(" ", devisible));
+        }
+    }
+}
