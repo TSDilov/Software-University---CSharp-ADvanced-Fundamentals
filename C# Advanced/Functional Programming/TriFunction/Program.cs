b3af530bf018019d67f3e66c524257821eb7ac57
TSDilov <cv.dilov@abv.bg>
2021-12-03 09:32:15 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/TriFunction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Func<string, int, bool> equalOrLargerThan = (name, asciiSum) => name.Sum(x => x) >= asciiSum;
+
+            Func<List<string>, Func<string, int, bool>, string> firstNameEqualOrLarger = (allNames, func)
+                => allNames.FirstOrDefault(x => func(x, n));
+
+            Console.WriteLine(firstNameEqualOrLarger(names, equalOrLargerThan));
+        }
+    }
+}
