d9959a0f256818c6e5d2b37eecb715a1494ceee3
TSDilov <cv.dilov@abv.bg>
2021-12-03 09:31:04 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThePartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            var filters = new Dictionary<string, Predicate<string>>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "Print")
+            {
+                string[] addRemoveFilter = input.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                string addRemove = addRemoveFilter[0];
+                string condition = addRemoveFilter[1];
+                string value = addRemoveFilter[2];
+                switch (addRemove)
+                {
+                    case "Add filter":
+                        Predicate<string> predicate = GetPredicate(condition, value);
+                        filters.Add(value, predicate);
+                        break;
+                    case "Remove filter":
+                        predicate = GetPredicate(condition, value);
+                        filters.Remove(value);
+                        break;
+                }
+
+            }
+            foreach (var filter in filters)
+            {
+                names.RemoveAll(filter.Value);
+            }
+            Console.WriteLine(string.Join(" ", names));
+
+        }
+        static Predicate<string> GetPredicate(string commandType, string arg) 
+        {
+            switch (commandType)
+            {
+                case "Starts with":
+                    return name => name.StartsWith(arg);
+                case "Ends with":
+                    return name => name.EndsWith(arg);
+                case "Contains":
+                    return name => name.Contains(arg);
+                case "Length":
+                    return name => name.Length == int.Parse(arg);
+                default:
+                    throw new ArgumentException("Invalid command type: " + commandType);
+            }
+        }
+        
+    }
+}
