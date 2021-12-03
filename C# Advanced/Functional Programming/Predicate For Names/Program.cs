cc487be0acbf38868d5375447c8af96766044719
TSDilov <cv.dilov@abv.bg>
2021-12-03 09:23:36 +0200

    Added C# Advance Functional Programming

---
+++ C# Advanced/Functional Programming/Predicate For Names/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split().ToList();
+            Predicate<string> function = word => word.Length <= n;
+            names = names.FindAll(function);
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
