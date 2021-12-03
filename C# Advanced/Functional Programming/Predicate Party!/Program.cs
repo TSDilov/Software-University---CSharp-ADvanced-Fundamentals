fdccad43001426c9f1002290b6718ffb26b7786a
TSDilov <cv.dilov@abv.bg>
2021-12-03 09:29:49 +0200

    Added C# Advanced Functional Programming code

---
+++ C# Advanced/Functional Programming/Predicate Party!/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateParty_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = Console.ReadLine().Split().ToList();
+            
+
+            string[] commands = Console.ReadLine().Split();
+
+            while (commands[0] != "Party!")
+            {
+                Predicate<string> predicate = GetPredicate(commands[1], commands[2]);
+                if (commands[0] == "Remove")
+                {
+                    people.RemoveAll(predicate);
+                }
+
+                else if (commands[0] == "Double")
+                {
+                    List<string> doubleNames = people.FindAll(predicate);
+
+                    if (doubleNames.Any())
+                    {
+                        int index = people.FindIndex(predicate);
+                        people.InsertRange(index, doubleNames);
+                    }
+
+                }
+                
+                commands = Console.ReadLine().Split();
+            }
+            if (people.Any())
+            {
+                Console.WriteLine($"{string.Join(", ", people)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+
+        private static Predicate<string> GetPredicate(string criteria, string paramForCriteria)
+        {
+            if (criteria == "StartsWith")
+            {
+                return x => x.StartsWith(paramForCriteria);
+            }
+            if (criteria == "EndsWith")
+            {
+                return x => x.EndsWith(paramForCriteria);
+            }
+            return x => x.Length == int.Parse(paramForCriteria);
+        }
+    }
+}
