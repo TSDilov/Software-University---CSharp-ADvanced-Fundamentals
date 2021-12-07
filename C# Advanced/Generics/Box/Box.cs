61abd68407431ea86209c218ca2b6595cdb9937a
TSDilov <cv.dilov@abv.bg>
2021-12-07 10:13:31 +0200

    Added C# Advanced OOP Generics code

---
+++ C# Advanced/Generics/Box/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box <T>
+    {
+        private List<T> myList = new List<T>();
+
+        public void Add(T element)
+        {
+            myList.Add(element);
+        }
+        public int Count => myList.Count;
+        public T Remove()
+        {
+            T element = myList[myList.Count - 1];
+            myList.RemoveAt(myList.Count - 1);
+            return element;
+        }
+        
+    }
+}
