8a741046df0cdc2fa8c193a1939b827c3232112d
TSDilov <cv.dilov@abv.bg>
2021-12-08 10:45:46 +0200

    Added C# Advanced OOP Generics code

---
+++ C# Advanced/Generics/Generic Box of String/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxofString
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+        public Box(T value)
+        {
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Value}";
+        }
+    }
+}
