267e818917eb58868dcdc0f56677395d7954d149
TSDilov <cv.dilov@abv.bg>
2021-12-09 10:30:46 +0200

    Added CE Advanced OOP Generics code

---
+++ C# Advanced/Generics/Generic Swap Method String/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> list = new List<Box<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = new Box<string>(Console.ReadLine());
+                list.Add(input);
+            }
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Swap(list, indexes[0], indexes[1]);
+            Console.WriteLine(string.Join(Environment.NewLine, list));
+        }
+        static void Swap<T>(List<Box<T>> list, int index1, int index2)
+        {
+            Box<T> temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+    }
+}
