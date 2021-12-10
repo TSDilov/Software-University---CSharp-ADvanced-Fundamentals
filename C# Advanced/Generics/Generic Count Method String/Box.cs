6b21ed91342e4213f89cfdc8390e1850d06ad3d8
TSDilov <cv.dilov@abv.bg>
2021-12-10 09:43:39 +0200

    Added C# Advanced OOP Generics

---
+++ C# Advanced/Generics/Generic Count Method String/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodString
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public T Value { get; set; }
+        private List<T> items;
+        public Box()
+        {
+            items = new List<T>();
+        }
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public int FilterCount(T item)
+        {
+            int count = 0;
+            foreach (var element in items)
+            {
+                if (element.CompareTo(item) > 0)
+                {
+                    count++;  
+                }
+            }
+            return count;
+        }        
+    }
+}
---
+++ C# Advanced/Generics/Generic Count Method String/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodString
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new Box<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+            string itemTocompare = Console.ReadLine();
+            Console.WriteLine(box.FilterCount(itemTocompare));
+        }
+        
+    }
+}
