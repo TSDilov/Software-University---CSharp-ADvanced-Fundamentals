0f715f1a8e56557e74b2596f8033a77e343c7719
TSDilov <cv.dilov@abv.bg>
2021-12-11 08:48:14 +0200

    Added C# Advanced OOP Generics code

---
+++ C# Advanced/Generics/Generic Count Method Double/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericCountMethodDouble
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new Box<double>();
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+            double forCompare = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.GreaterCount(forCompare));
+        }
+    }
+}
