713215062438f1847ab8419821ed1ba95458a4f0
TSDilov <cv.dilov@abv.bg>
2021-12-11 08:50:32 +0200

    Added C# Advanced OOP Generics code

---
+++ C# Advanced/Generics/Tuple/MyTuple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class MyTuple<Item1, Item2>
+    {
+        public MyTuple(Item1 firstItem, Item2 secondItem)
+        {
+            FirstItem = firstItem;
+            SecondItem = secondItem;
+        }
+        public Item1 FirstItem { get; set; }
+        public Item2 SecondItem { get; set; }
+        public string GetItem()
+        {
+            return $"{FirstItem} -> {SecondItem}";
+        }
+
+    }
+}
