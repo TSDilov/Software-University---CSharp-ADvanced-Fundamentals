253661eecc9ab8d535a6e5938513cb08b6f02597
TSDilov <cv.dilov@abv.bg>
2021-12-11 08:47:07 +0200

    Added C# Advanced OOP Generics code

---
+++ C# Advanced/Generics/Generic Count Method Double/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodDouble
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        private List<T> items;
+        public Box()
+        {
+            items = new List<T>();
+        }
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public int GreaterCount(T item)
+        {
+            int count = 0;
+            foreach (var element in items)
+            {
+                if (element.CompareTo(item) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
