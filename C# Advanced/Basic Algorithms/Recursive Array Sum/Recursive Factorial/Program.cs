7c4611ba6cf950138a14f8f7d946932dc51cd984
TSDilov <cv.dilov@abv.bg>
2021-12-15 11:48:47 +0200

    Added C# Advanced Basic Algorithms code

---
+++ C# Advanced/Basic Algorithms/Recursive Array Sum/Recursive Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int factorial = GetFactorial(n);
+            Console.WriteLine(factorial);
+        }
+
+        private static int GetFactorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * GetFactorial(n - 1);
+        }
+    }
+}
