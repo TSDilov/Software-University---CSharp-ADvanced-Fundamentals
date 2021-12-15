6aface5876c035e68d443c49df8d60442d108d31
TSDilov <cv.dilov@abv.bg>
2021-12-15 11:47:19 +0200

    Added C# Advanced Basic Algorithms code

---
+++ C# Advanced/Basic Algorithms/Recursive Array Sum/Recursive Array Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = SumOfArray(numbers, 0);
+            Console.WriteLine(sum);
+        }
+
+        private static int SumOfArray(int[] numbers, int index)
+        {
+            if (index == numbers.Length)
+            {
+                return 0;
+            }
+            return numbers[index] + SumOfArray(numbers, index + 1);
+        }
+    }
+}
