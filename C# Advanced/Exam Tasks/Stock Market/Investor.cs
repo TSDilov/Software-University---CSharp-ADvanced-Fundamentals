5a9c6979af6fe67ff8e857b3e8125cd9a78ad193
TSDilov <cv.dilov@abv.bg>
2021-12-16 12:54:36 +0200

    Added C# Advanced Exam Tasks code

---
+++ C# Advanced/Exam Tasks/Stock Market/Investor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+            portfolio = new List<Stock>();
+        }
+        private List<Stock> portfolio;
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public int Count => portfolio.Count;
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && MoneyToInvest > stock.PricePerShare)
+            {
+                portfolio.Add(stock);
+                MoneyToInvest -= stock.PricePerShare;
+            }
+        }
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            var company = portfolio.FirstOrDefault(c => c.CompanyName == companyName);
+            if (company == null)
+            {
+                return $"{companyName} does not exist.";
+            }
+            else
+            {
+                if (sellPrice < company.PricePerShare)
+                {
+                    return $"Cannot sell {companyName}.";
+                }
+                else
+                {
+                    portfolio.Remove(company);
+                    MoneyToInvest += sellPrice;
+                    return $"{companyName} was sold.";
+                }
+            }
+        }
+        public Stock FindStock(string companyName)
+        {
+            return portfolio.FirstOrDefault(c => c.CompanyName == companyName);
+        }
+        public Stock  FindBiggestCompany()
+        {
+            return portfolio.OrderByDescending(c => c.MarketCapitalization).FirstOrDefault();
+        }
+        public string InvestorInformation()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            foreach (var stock in portfolio)
+            {
+                sb.AppendLine(stock.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
