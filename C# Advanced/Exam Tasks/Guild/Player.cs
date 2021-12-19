b392a74ba42bcac680c66dce190271b20d1ce51e
TSDilov <cv.dilov@abv.bg>
2021-12-19 15:31:43 +0200

    Added C# Advanced Exam tasks code

---
+++ C# Advanced/Exam Tasks/Guild/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public Player(string name, string myClass)
+        {
+            Name = name;
+            MyClass = myClass;
+        }
+        public string Name { get; set; }
+        public string MyClass { get; set; }
+        public string Rank { get; set; } = "Trial";
+        public string Description { get; set; } = "n/a";
+
+        public override string ToString()
+        {
+            return @$"Player {Name}: {MyClass}
+Rank: {Rank}
+Description: {Description}";
+        }
+    }
+}
