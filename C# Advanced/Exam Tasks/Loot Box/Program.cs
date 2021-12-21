e9f07da5c42101f4718954647430f152828b7aa2
TSDilov <cv.dilov@abv.bg>
2021-12-21 10:27:19 +0200

    Added C# Advanced Exam tasks code

---
+++ C# Advanced/Exam Tasks/Loot Box/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LootBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstBox = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            var secondBox = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            var claimedItems = new List<int>();
+
+            while (firstBox.Count > 0 && secondBox.Count > 0)
+            {
+                int currentItemFromFirst = firstBox.Peek();
+                var currentItemFromSecond = secondBox.Peek();
+                var sum = currentItemFromFirst + currentItemFromSecond;
+                if (sum % 2 == 0)
+                {
+                    claimedItems.Add(sum);
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    secondBox.Pop();
+                    firstBox.Enqueue(currentItemFromSecond);
+                }
+            }
+            if (firstBox.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+                if (claimedItems.Sum() >= 100)
+                {
+                    Console.WriteLine($"Your loot was epic! Value: {claimedItems.Sum()}");
+                }
+                else
+                {
+                    Console.WriteLine($"Your loot was poor... Value: {claimedItems.Sum()}");
+                }
+            }
+            if (secondBox.Count == 0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+                if (claimedItems.Sum() >= 100)
+                {
+                    Console.WriteLine($"Your loot was epic! Value: {claimedItems.Sum()}");
+                }
+                else
+                {
+                    Console.WriteLine($"Your loot was poor... Value: {claimedItems.Sum()}");
+                }
+            }
+        }
+    }
+}
