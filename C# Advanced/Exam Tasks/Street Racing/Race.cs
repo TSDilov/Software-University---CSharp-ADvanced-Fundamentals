51c7bbbcc196e75cee5e6bb053869fe045ca45a4
TSDilov <cv.dilov@abv.bg>
2021-12-22 13:43:45 +0200

    Added C# Advanced Exam tasks code

---
+++ C# Advanced/Exam Tasks/Street Racing/Race.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxHorsePower;
+            Participants = new List<Car>();
+        }
+        public List<Car> Participants { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+        public int Count => Participants.Count;
+        public void Add(Car car)
+        {
+            if (!Participants.Any(c => c.LicensePlate == car.LicensePlate) && Participants.Count < Capacity
+                && car.HorsePower <= MaxHorsePower)
+            {
+                Participants.Add(car);
+            }
+        }
+        public bool Remove(string licensePlate)
+        {
+            var car = Participants.FirstOrDefault(c => c.LicensePlate == licensePlate);
+            if (car == null)
+            {
+                return false;
+            }
+            else
+            {
+                Participants.Remove(car);
+                return true;
+            }
+        }
+        public Car FindParticipant(string licensePlate)
+        {
+            return Participants.FirstOrDefault(c => c.LicensePlate == licensePlate);
+        }
+        public Car GetMostPowerfulCar()
+        {
+            return Participants.OrderByDescending(c => c.HorsePower).FirstOrDefault();
+        }
+        public string Report()
+        {
+            StringBuilder report = new StringBuilder();
+            report.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+            foreach (var car in Participants)
+            {
+                report.AppendLine(car.ToString());
+            }
+            return report.ToString().TrimEnd();
+        }
+    }
+}
