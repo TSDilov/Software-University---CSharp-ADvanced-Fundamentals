c1750143e9d841949e442ac98a2b3f0f42e2825d
TSDilov <cv.dilov@abv.bg>
2021-12-24 14:08:11 +0200

    Added C# Advanced Exam tasks code

---
+++ C# Advanced/Exam Tasks/The Race/Race.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        public Race(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            racers = new List<Racer>();
+        }
+        private List<Racer> racers;
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => racers.Count;
+        public void Add(Racer Racer)
+        {
+            if (racers.Count < Capacity)
+            {
+                racers.Add(Racer);
+            }
+        }
+        public bool Remove(string name)
+        {
+            var racer = racers.FirstOrDefault(r => r.Name == name);
+            if (racer == null)
+            {
+                return false;
+            }
+            else
+            {
+                racers.Remove(racer);
+                return true;
+            }
+        }
+        public Racer GetOldestRacer()
+        {
+            return racers.OrderByDescending(r => r.Age).FirstOrDefault();
+        }
+        public Racer GetRacer(string name)
+        {
+            return racers.FirstOrDefault(r => r.Name == name);
+        }
+        public Racer GetFastestRacer()
+        {
+            return racers.OrderByDescending(r => r.Car.Speed).FirstOrDefault();
+        }
+        public string Report()
+        {
+            var report = new StringBuilder();
+            report.AppendLine($"Racers participating at {Name}:");
+            foreach (var racer in racers)
+            {
+                report.AppendLine(racer.ToString());
+            }
+            return report.ToString().TrimEnd();
+        }
+    }
+}
