477beb89df4e9e37b7b214826713b2c39a5d6abb
TSDilov <cv.dilov@abv.bg>
2021-12-29 13:34:33 +0200

    Added C# Advanced OOp Inheritance code

---
+++ C# Advanced OOP/Inheritance/Random List/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        
+        
+        public string RandomString()
+        {
+            var index = new Random().Next(0, Count);
+            var removed = this[index];
+            RemoveAt(index);
+            return removed;
+        }
+        
+    }
+}
---
+++ C# Advanced OOP/Inheritance/Random List/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var list = new RandomList();
+            list.Add("aaa");
+            list.Add("bbb");
+            list.Add("ccc");
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
