9560cf9914bdaec52b007efd77d3a86e657ab837
TSDilov <cv.dilov@abv.bg>
2022-01-03 11:09:51 +0200

    Added C# Advanced OOP Inheritance code

---
+++ C# Advanced OOP/Inheritance/Need For Speed/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double defaultFuelConsumprion = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+            this.FuelConsumption = defaultFuelConsumprion;
+        }
+        public double DefaultFuelConsumption { get; set; } 
+        public virtual double FuelConsumption { get; set; }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * FuelConsumption;
+        }
+        
+    }
+}
