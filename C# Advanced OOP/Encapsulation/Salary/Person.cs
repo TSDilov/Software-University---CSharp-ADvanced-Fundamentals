6a5ff847f8e042daa5bcc4f0b6cf63184cb106fe
TSDilov <cv.dilov@abv.bg>
2022-01-06 10:56:30 +0200

    Added C# Advanced OOP Encapsulation code

---
+++ C# Advanced OOP/Encapsulation/Salary/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; private set; }
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage /= 2.0M;
+            }
+            this.Salary *= 1 + (percentage / 100);
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
