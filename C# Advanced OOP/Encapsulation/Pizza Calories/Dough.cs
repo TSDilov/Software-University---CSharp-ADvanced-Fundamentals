33f4dcf85b8010de8a1b47321bbbd6e278006f43
TSDilov <cv.dilov@abv.bg>
2022-01-09 12:24:29 +0200

    Added C# Advanced OOP Encapsulation code

---
+++ C# Advanced OOP/Encapsulation/Pizza Calories/Dough.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Dough
+    {
+        private Dictionary<string, double> flourCalories = new Dictionary<string, double>
+        {
+            { "white", 1.5 },
+            { "wholegrain", 1 }
+        };
+        private Dictionary<string, double> bakingTechniqueCalories = new Dictionary<string, double>
+        {
+            { "crispy", 0.9 },
+            { "chewy", 1.1},
+            { "homemade", 1}
+        };
+        private string flourType;
+        private string bakingTechnique;
+        private double weight;
+        public Dough(string flourType, string bakingTechnique, double weight)
+        {
+            this.FlourType = flourType;
+            this.BakingTechnique = bakingTechnique;
+            this.Weight = weight;
+        }
+        public string FlourType
+        {
+            get => this.flourType; 
+            private set 
+            {
+                if (!flourCalories.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+                this.flourType = value; 
+            }
+        }
+        public string BakingTechnique
+        {
+            get => this.bakingTechnique; 
+            private set 
+            {
+                if (!bakingTechniqueCalories.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+                this.bakingTechnique = value; 
+            }
+        }
+        public double Weight
+        {
+            get => this.weight; 
+            private set 
+            {
+                if (value < 1 || value > 200)
+                {
+                    throw new ArgumentException("Dough weight should be in the range[1..200].");
+                }
+                this.weight = value; 
+            }
+        }
+        public double Calories 
+            => 2 * Weight * this.flourCalories[this.FlourType.ToLower()] * this.bakingTechniqueCalories[this.BakingTechnique.ToLower()];
+    }
+}
---
+++ C# Advanced OOP/Encapsulation/Pizza Calories/Pizza.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Pizza
+    {
+        private string name;
+        private Dough dough;
+        private List<Topping> toppings;
+        public Pizza(string name)
+        {
+            this.Name = name;
+            toppings = new List<Topping>();
+        }
+        public string Name
+        {
+            get => this.name;
+            private set 
+            {
+                if (value.Length < 1 || value.Length > 15)
+                {
+                    throw new ArgumentException("Pizza name should be between 1 and 15 symbols.");
+                }
+                name = value; 
+            }
+        }
+        public Dough Dough
+        {
+            get { return dough; }
+            set { dough = value; }
+        }
+        public double Calories => this.CalculateCalories();
+        public void AddTopping(Topping topping)
+        {
+            if (toppings.Count > 10)
+            {
+                throw new ArgumentException("Number of toppings should be in range [0..10].");
+            }
+            this.toppings.Add(topping);
+        }
+        
+        private double CalculateCalories()
+        {
+            double totalCalories = this.dough.Calories;
+            foreach (var topping in toppings)
+            {
+                totalCalories += topping.Calories;
+            }
+            return totalCalories;
+        }
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.Calories:F2} Calories.";
+        }
+    }
+}
