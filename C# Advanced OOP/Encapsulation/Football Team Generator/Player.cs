927b6353069c029b3bdf7f4f472f85558de3eda3
TSDilov <cv.dilov@abv.bg>
2022-01-11 11:05:40 +0200

    Added C# Advanced Encapsulation code

---
+++ C# Advanced OOP/Encapsulation/Football Team Generator/Player.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public class Player
+    {
+        private string name;
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+        public Player(string name, int endurance, int sprint, int dribble, int passing, int shooting)
+        {
+            this.Name = name;
+            this.Endurance = endurance;
+            this.Sprint = sprint;
+            this.Dribble = dribble;
+            this.Passing = passing;
+            this.Shooting = shooting;
+        }
+        public string Name
+        {
+            get => this.name;
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                this.name = value; 
+            }
+        }
+        public double Stats => (this.Endurance + this.Sprint + this.Dribble + this.Passing + this.Shooting) / 5.0;
+        public int Endurance 
+        { 
+            get => this.endurance;
+            private set
+            {
+                ValidateStat(nameof(this.Endurance),value);
+                this.endurance = value;
+            }
+        }
+
+        public int Sprint 
+        { 
+            get => this.sprint;
+            private set
+            {
+                ValidateStat(nameof(this.Sprint), value);
+                this.sprint = value;
+            }
+        }
+        public int Dribble
+        {
+            get => this.dribble;
+            private set
+            {
+                ValidateStat(nameof(this.Dribble), value);
+                this.dribble = value;
+            }
+        }
+        public int Passing
+        {
+            get => this.passing;
+            private set
+            {
+                ValidateStat(nameof(this.Passing), value);
+                this.passing = value;
+            }
+        }
+        public int Shooting
+        {
+            get => this.shooting;
+            private set
+            {
+                ValidateStat(nameof(this.Shooting), value);
+                this.shooting = value;
+            }
+        }
+        private void ValidateStat(string name, int value)
+        {
+            if (value < 0 || value > 100)
+            {
+                throw new ArgumentException($"{name} should be between 0 and 100.");
+            }
+        }
+    }
+}
---
+++ C# Advanced OOP/Encapsulation/Football Team Generator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FootballTeamGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Team> teams = new Dictionary<string, Team>();
+            var command = Console.ReadLine().Split(";");
+
+            while (command[0] != "END")
+            {
+                var currentAction = command[0];
+                try
+                {
+                    if (currentAction == "Team")
+                    {
+                        var nameOfTeam = command[1];
+                        var team = new Team(nameOfTeam);
+                        teams.Add(nameOfTeam, team);
+                    }
+                    else if (currentAction == "Add")
+                    {
+                        var nameOfTeam = command[1];
+                        if (!teams.ContainsKey(nameOfTeam))
+                        {
+                            Console.WriteLine($"Team {nameOfTeam} does not exist.");
+                            command = Console.ReadLine().Split(";");
+                            continue;
+                        }
+                        var playerName = command[2];
+                        int endurance = int.Parse(command[3]);
+                        int sprint = int.Parse(command[4]);
+                        int dribble = int.Parse(command[5]);
+                        int passing = int.Parse(command[6]);
+                        int shooting = int.Parse(command[7]);
+                        var player = new Player(playerName, endurance, sprint, dribble, passing, shooting);
+                        teams[nameOfTeam].AddPlayer(player);
+                    }
+                    else if (currentAction == "Remove")
+                    {
+                        var nameOfTeam = command[1];
+                        var playerName = command[2];
+                        teams[nameOfTeam].RemovePlayer(playerName);
+                    }
+                    else if (currentAction == "Rating")
+                    {
+                        var nameOfTeam = command[1];
+                        if (!teams.ContainsKey(nameOfTeam))
+                        {
+                            Console.WriteLine($"Team {nameOfTeam} does not exist.");
+                            command = Console.ReadLine().Split(";");
+                            continue;
+                        }
+                        Console.WriteLine($"{nameOfTeam} - {teams[nameOfTeam].Rating}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                command = Console.ReadLine().Split(";");
+            }
+        }
+    }
+}
