44fbf4b5d1deb8330578b174c4e9ad86810b3ec3
TSDilov <cv.dilov@abv.bg>
2022-01-14 10:53:21 +0200

    Added C# Advanced OOP Interfaces and Abstraction code

---
+++ C# Advanced OOP/Interfaces and Abstraction/Telephony/StationaryPhone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony
+{
+    public class StationaryPhone : ICalleble
+    {
+        public StationaryPhone(string phoneNumber)
+        {
+            this.PhoneNumber = phoneNumber;
+        }
+
+        public string PhoneNumber { get; private set; }
+
+        public string Call(string phoneNumber)
+        {
+            if (this.IsNumberValid(phoneNumber))
+            {
+                return $"Dialing... {phoneNumber}";
+            }
+            return "Invalid number!";
+        }
+        private bool IsNumberValid(string phoneNumber)
+        {
+            for (int i = 0; i < phoneNumber.Length; i++)
+            {
+                if (!char.IsDigit(phoneNumber[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
