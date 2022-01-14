64a56894c780fb7eb33bb60ee3c063b39a1c36d9
TSDilov <cv.dilov@abv.bg>
2022-01-14 10:48:33 +0200

    Added C# advanced OOP Interfaces and Abstraction code

---
+++ C# Advanced OOP/Interfaces and Abstraction/Telephony/ICalleble.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony
+{
+    public interface ICalleble
+    {
+        public string PhoneNumber { get; }
+        string Call(string phoneNumber);
+    }
+}
