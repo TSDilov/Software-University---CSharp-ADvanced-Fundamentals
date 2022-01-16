bf05a27b27a028c2495326f70e28327aa75f5d51
TSDilov <cv.dilov@abv.bg>
2022-01-16 16:17:49 +0200

    Added C# Advanced OOP Interfaces and Abstraction code

---
+++ C# Advanced OOP/Interfaces and Abstraction/Military Elite/Mission.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite
+{
+    public class Mission
+    {
+        private string state;
+        public Mission(string state, string codeName)
+        {
+            this.State = state;
+            this.CodeName = codeName;
+        }
+        public string CodeName { get; set; }
+        public string State 
+        {
+            get => this.state;
+            set
+            {
+                if (value != "Finished" && value != "inProgress")
+                {
+                    throw new ArgumentException("Invalid mission state");
+                }
+                this.state = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Code Name: {this.CodeName} State: {this.State}";
+        }
+    }
+}
