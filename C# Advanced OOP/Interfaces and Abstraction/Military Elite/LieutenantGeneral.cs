32a4928157cf86f805cf41956016701d30802075
TSDilov <cv.dilov@abv.bg>
2022-01-16 16:16:31 +0200

    Added C# Advanced OOP Interfaces and Abstraction code

---
+++ C# Advanced OOP/Interfaces and Abstraction/Military Elite/LieutenantGeneral.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite
+{
+    public class LieutenantGeneral : Private, ILieutenantGeneral
+    {
+        public LieutenantGeneral(string id, string firstName, string lastName, decimal salary)
+            : base(id, firstName, lastName, salary)
+        {
+            this.PrivatesUnderCommand = new List<Private>();
+        }
+        public List<Private> PrivatesUnderCommand { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ToString());
+            sb.AppendLine("Privates:");
+            foreach (var _private in PrivatesUnderCommand)
+            {
+                sb.AppendLine(" " + _private);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
