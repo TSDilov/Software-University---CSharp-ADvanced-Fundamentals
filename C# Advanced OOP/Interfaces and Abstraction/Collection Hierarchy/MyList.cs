bce0af5be6ce4fd6db99a5b41e28231e86cb4da8
TSDilov <cv.dilov@abv.bg>
2022-01-17 10:27:43 +0200

    Added C# Advanced OOP Interfaces and Abstraction code

---
+++ C# Advanced OOP/Interfaces and Abstraction/Collection Hierarchy/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionHierarchy
+{
+    public class MyList : IAdd, IRemove
+    {
+        private List<string> elements;
+
+        public MyList()
+        {
+            this.elements = new List<string>();
+        }
+        public int Used => this.elements.Count;
+
+        public int Add(string element)
+        {
+            elements.Insert(0, element);
+            return 0;
+        }
+
+        public string Remove()
+        {
+            var element = elements[0];
+            elements.RemoveAt(0);
+            return element;
+        }
+    }
+}
---
+++ C# Advanced OOP/Interfaces and Abstraction/Collection Hierarchy/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionHierarchy
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var addCollection = new AddCollection();
+            var addRemoveCollection = new AddRemoveCollection();
+            var myList = new MyList();
+            var inputString = Console.ReadLine().Split();
+            var indexesInAddCollection = new List<int>();
+            var indexesIntAddRemoveCollection = new List<int>();
+            var indexesInMyList = new List<int>();
+            foreach (var word in inputString)
+            {
+                indexesInAddCollection.Add(addCollection.Add(word));
+                indexesIntAddRemoveCollection.Add(addRemoveCollection.Add(word));
+                indexesInMyList.Add(myList.Add(word));
+            }
+            var numberOfRemoveOperations = int.Parse(Console.ReadLine());
+            var removedFromAddRemoveCollection = new List<string>();
+            var removedFromMyList = new List<string>();
+            for (int i = 0; i < numberOfRemoveOperations; i++)
+            {
+                removedFromAddRemoveCollection.Add(addRemoveCollection.Remove());
+                removedFromMyList.Add(myList.Remove());
+            }
+
+            Console.WriteLine(string.Join(" ", indexesInAddCollection));
+            Console.WriteLine(string.Join(" ", indexesIntAddRemoveCollection));
+            Console.WriteLine(string.Join(" ", indexesInMyList));
+            Console.WriteLine(string.Join(" ", removedFromAddRemoveCollection));
+            Console.WriteLine(string.Join(" ", removedFromMyList));
+        }
+    }
+}
