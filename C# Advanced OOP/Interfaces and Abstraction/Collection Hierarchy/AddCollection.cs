516b7692bcda79350b9b734b650b0ff4f2122b60
TSDilov <cv.dilov@abv.bg>
2022-01-17 10:26:49 +0200

    Added C# Advanced OOP Interfaces and Abstraction code

---
+++ C# Advanced OOP/Interfaces and Abstraction/Collection Hierarchy/AddCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionHierarchy
+{
+    public class AddCollection : IAdd
+    {
+        private List<string> elements;
+
+        public AddCollection()
+        {
+            this.elements = new List<string>();
+        }
+
+        public int Add(string element)
+        {
+            elements.Add(element);
+            return elements.Count - 1;
+        }
+    }
+}
---
+++ C# Advanced OOP/Interfaces and Abstraction/Collection Hierarchy/AddRemoveCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionHierarchy
+{
+    public class AddRemoveCollection : IAdd, IRemove
+    {
+        private List<string> elements;
+
+        public AddRemoveCollection()
+        {
+            this.elements = new List<string>();
+        }
+
+        public int Add(string element)
+        {
+            elements.Insert(0, element);
+            return 0;
+        }
+
+        public string Remove()
+        {
+            var element = elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+            return element;
+        }
+    }
+}
