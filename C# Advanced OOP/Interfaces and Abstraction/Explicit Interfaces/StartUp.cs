c27a0c2ca095f38ab3f04c4e0539b7d7b7788a63
TSDilov <cv.dilov@abv.bg>
2022-01-17 10:30:20 +0200

    Added C# Advanced OOP Interfaces and Abstraction code

---
+++ C# Advanced OOP/Interfaces and Abstraction/Explicit Interfaces/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+
+            while (input[0] != "End")
+            {
+                var name = input[0];
+                var country = input[1];
+                var age = int.Parse(input[2]);
+                var currentCitizen = new Citizen(name, country, age);
+                Console.WriteLine(currentCitizen);
+                input = Console.ReadLine().Split();
+            }
+        }
+    }
+}
