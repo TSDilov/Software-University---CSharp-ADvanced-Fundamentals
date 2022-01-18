d2f876a9a82c4b5f7271bd85b574ff4af2901798
TSDilov <cv.dilov@abv.bg>
2022-01-18 10:13:59 +0200

    Added C# Advanced OOP Polymorphism code

---
+++ C# Advanced OOP/Polymorphism/Shapes/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Shapes
+
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(3, 6);
+            Circle circle = new Circle(5);
+
+            Console.WriteLine(rectangle.Draw());
+            var rectanglePerimeter = rectangle.CalculatePerimeter();
+            Console.WriteLine($"{rectanglePerimeter:F2}");
+            var rectangleArea = rectangle.CalculateArea();
+            Console.WriteLine($"{rectangleArea:F2}");
+
+            Console.WriteLine(circle.Draw());
+            var circlePerimeter = circle.CalculatePerimeter();
+            Console.WriteLine($"{circlePerimeter:F2}");
+            var circleArea = circle.CalculateArea();
+            Console.WriteLine($"{circleArea:F2}");
+        }
+    }
+}
