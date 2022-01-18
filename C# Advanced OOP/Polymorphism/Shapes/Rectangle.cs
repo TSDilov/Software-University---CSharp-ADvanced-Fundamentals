5e620ad31e5d88aa6c1f5b0422c8e070aae1d7f2
TSDilov <cv.dilov@abv.bg>
2022-01-18 10:13:28 +0200

    Added C# Advanced OOP Polymorphism code

---
+++ C# Advanced OOP/Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height { get; protected set; }
+        public double Width { get; protected set; }
+
+        public override double CalculatePerimeter()
+            => (Height + Width) * 2;
+
+        public override double CalculateArea()
+            => Height * Width;
+
+        public override string Draw()
+            => base.Draw() + GetType().Name;
+    }
+}
