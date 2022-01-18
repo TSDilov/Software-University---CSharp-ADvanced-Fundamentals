6d91adabcb320afb118d3adb12058c5f2832740c
TSDilov <cv.dilov@abv.bg>
2022-01-18 10:11:29 +0200

    Added C# Advanced OOP Polymorphism code

---
+++ C# Advanced OOP/Polymorphism/Math Operations/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var firstNumber = Console.ReadLine();
+            var secondNumber = Console.ReadLine();
+            var thirdNumber = Console.ReadLine();
+
+            var mathOperation = new MathOperations();
+            Console.WriteLine(mathOperation.Add(int.Parse(firstNumber), int.Parse(secondNumber)));
+            Console.WriteLine(mathOperation.Add(double.Parse(firstNumber), double.Parse(secondNumber), double.Parse(thirdNumber)));
+            Console.WriteLine(mathOperation.Add(decimal.Parse(firstNumber), decimal.Parse(secondNumber), decimal.Parse(thirdNumber)));
+        }
+    }
+}
