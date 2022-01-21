2445db09932f65f506322385afde6bc6a46afeee
TSDilov <cv.dilov@abv.bg>
2022-01-21 10:09:53 +0200

    Added C# Advanced OOP Reflection and Attributes code

---
+++ C# Advanced OOP/Reflection and Attributes/Stealer/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            var classFields = classType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            var sb = new StringBuilder();
+            sb.AppendLine($"Class under investigation: {investigatedClass}");
+            var instance = Activator.CreateInstance(classType, new object[] { });
+
+            foreach (var field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
