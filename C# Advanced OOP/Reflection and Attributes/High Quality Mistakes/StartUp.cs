81a8bc8526d8f2f5820c558bcf67fb04d4f67a7f
TSDilov <cv.dilov@abv.bg>
2022-01-21 10:11:55 +0200

    Added C# Advanced OOP Reflection and Attributes code

---
+++ C# Advanced OOP/Reflection and Attributes/High Quality Mistakes/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var spy = new Spy();
+            var result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
