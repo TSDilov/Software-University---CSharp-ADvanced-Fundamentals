f2dd0e34017d1887088fe246e2dfbf0ae26fc4a3
TSDilov <cv.dilov@abv.bg>
2022-01-21 10:10:36 +0200

    Added C# Advanced OOP Reflection and Attributes code

---
+++ C# Advanced OOP/Reflection and Attributes/Stealer/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var spy = new Spy();
+            var result = spy.StealFieldInfo("Stealer.StartUp", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
