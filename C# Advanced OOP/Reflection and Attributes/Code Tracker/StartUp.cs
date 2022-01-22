2ce5f46de6f25345cdcdcee2e6cae981ef160623
TSDilov <cv.dilov@abv.bg>
2022-01-22 13:24:16 +0200

    Added C# Advanced OOP Reflection and Attributes code

---
+++ C# Advanced OOP/Reflection and Attributes/Code Tracker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Dilov")]
+    public class StartUp
+    {
+        [Author("Tsvetelin")]
+        public static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor("Tsvetelin");
+        }
+    }
+}
---
+++ C# Advanced OOP/Reflection and Attributes/Code Tracker/Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor(string name)
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance 
+                    | BindingFlags.Static | BindingFlags.NonPublic))
+                {
+                    var attribute = method.GetCustomAttribute<AuthorAttribute>();
+                    if (attribute != null && attribute.Name == name)
+                    {
+                        Console.WriteLine($"{method.Name} is writen by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
