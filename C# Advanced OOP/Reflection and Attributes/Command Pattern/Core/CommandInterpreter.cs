59da38d3f5fa7b1a5795f23a7d33d4e4d69fdcf2
TSDilov <cv.dilov@abv.bg>
2022-01-23 11:57:50 +0200

    Added C# Advanced OOP Reflection and Attributes code

---
+++ C# Advanced OOP/Reflection and Attributes/Command Pattern/Core/CommandInterpreter.cs
@@ -0,0 +1,33 @@
+﻿using CommandPattern.Core.Commands;
+using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            var input = args.Split();
+            var commandName = input[0] + "Command";
+            var arguments = input.Skip(1).ToArray();
+            
+            var type = Assembly.GetCallingAssembly()
+                .GetTypes()
+                .Where(t => t.Name == commandName)
+                .FirstOrDefault();
+            if (type == null)
+            {
+                throw new InvalidOperationException("Invalid command");
+            }
+
+            ICommand command = (ICommand)Activator.CreateInstance(type);
+            var result = command.Execute(arguments);
+            return result;
+        }
+    }
+}
---
+++ C# Advanced OOP/Reflection and Attributes/Command Pattern/Core/Engine.cs
@@ -0,0 +1,36 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    var input = Console.ReadLine();
+                    var result = this.commandInterpreter.Read(input);
+                    if (result == null)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(result);
+                }
+                catch (InvalidOperationException opEx)
+                {
+                    Console.WriteLine(opEx.Message);
+                };
+            }
+        }
+    }
+}
