9bca8e4c24745068207729e76cce4aa45c32305c
TSDilov <cv.dilov@abv.bg>
2022-01-24 10:39:16 +0200

    Added C# Advanced OOP Reflection and Attributes code

---
+++ C# Advanced OOP/Reflection and Attributes/Validation Attributes/MyRangeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minValue = 12;
+        private int maxValue = 90;
+        public override bool IsValid(object obj)
+        {
+            var ageOfPerson = (int)obj;
+            if (ageOfPerson < minValue || ageOfPerson > maxValue)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
