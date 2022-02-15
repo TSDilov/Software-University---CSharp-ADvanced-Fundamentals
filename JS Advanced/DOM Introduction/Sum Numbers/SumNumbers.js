b2f9a7dbd2fef3b1d7b2b3491fb6c5f480ed979e
TSDilov <cv.dilov@abv.bg>
2022-02-15 10:08:05 +0200

    Added JS Advanced DOM Introduction code

---
+++ JS Advanced/DOM Introduction/Sum Numbers/SumNumbers.js
@@ -0,0 +1,5 @@
+function calc() {
+    let firstInputElement = Number(document.getElementById('num1').value);
+    let secondInputElement = Number(document.getElementById('num2').value);
+    document.getElementById('sum').value = firstInputElement + secondInputElement;
+}
