ac90dac94850d950a01d6f3b5e91910ea98222b2
TSDilov <cv.dilov@abv.bg>
2022-02-17 10:25:22 +0200

    Added JS Advanced DOM Introduction code

---
+++ JS Advanced/DOM Introduction/Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let hiddenTextElement = document.getElementById('text');
+    hiddenTextElement.style.display = 'inline';
+    let linkClickedElement = document.getElementById('more');
+    linkClickedElement.style.display = 'none';
+}
